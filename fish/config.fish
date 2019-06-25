fd9ae21b197f291746a0960b2c0501859e0e58ab
kaile256 <46470475+kaile256@users.noreply.github.com>
2019-06-25 17:23:10 +0900

    Update config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,25 +1,11 @@
+## Never write "exec tmux" on script, or never access the WSL Distribution.
 #. expand(~/.config/fish/myconf.d/*.fish)
 #. expand(~/.config/fish/myfuctions/*.fish)
 
-#set -x TMUX /home/linuxbrew/.linuxbrew/bin/tmux
-#set -g fish_user_paths /home/linuxbrew/.linuxbrew/sbin $fish_user_paths
-set -x TERM screen-256color
-
-## Never write "exec tmux" on script, or never access the WSL Distribution.
-
-## Plugins; fzf
-# cf. ~/.config/fish/conf.d/fzf.fish
-## Plugins; ghq
-# set -g GHQ_SELECTOR fzf # Default selector is fzf
-
-## helps to let PATH for SDKMAN
-## sdk help > /dev/null
-
-# ## Config Plugins
-# for plugins_fisher in ~/.config/fish/myconf.fish/*.fish
-#     source $plugins_fisher
-# end
 umask 022
+
+### PATH
 set -g fish_user_paths "/home/linuxbrew/.linuxbrew/bin" $fish_user_paths
-status --is-interactive; and source (jump shell fish | psub)
+#set -x TMUX /home/linuxbrew/.linuxbrew/bin/tmux
+set -x TERM screen-256colorstatus --is-interactive; and source (jump shell fish | psub)
 
