8107e445fb8fb32733fcf5ef2d5fef055d12cdbc
kaile256 <kaile256acc@gmail.com>
2019-06-30 07:44:33 +0900

    fish: prompt a little bug

--- fish/myfuctions/fish_prompt.fish
+++ fish/myfuctions/fish_prompt.fish
@@ -53,7 +53,7 @@ function fish_prompt
     set -l git_branch (_git_branch_name)
 
     if [ (_git_is_dirty) ]
-      set git_info $brred $git_branch$normal
+      set git_info $brred $git_branch $normal
       #set git_info $brred $git_branch "±" $normal
     else
       set git_info $brgreen $git_branch $normal
