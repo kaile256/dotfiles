f6dad0d381dd8a539af32165acfe2a57aa841148
kaile256 <kaile256acc@gmail.com>
2019-07-11 13:39:36 +0900

    overwrite #!

--- symlink.sh
+++ symlink.sh
@@ -1,4 +1,4 @@
-#! /bin/bash
+#! /usr/bin/env bash
 
 # C-option: do not overwrite on redirect; replace '>' with '>|' to surely overwite a file.
 # e-option: stop if error (non 0); add '&& true' to continue even when error.
