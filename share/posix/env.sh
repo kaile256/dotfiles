7e999330498bf71163b5365f4cd05940fc87ffb2
kaile256 <kaile256acc@gmail.com>
2019-07-15 17:23:50 +0900

    set apart those env-variables unconcerned with xdg base dir specifications

--- share/posix/env.sh
+++ share/posix/env.sh
@@ -1,37 +1,4 @@
-## shared by posix compatible
-
-export XDG_CONFIG_HOME=$HOME/.config
-export XDG_CACHE_HOME=$HOME/.cache
-export XDG_DATA_HOME=$HOME/.local/share
-
-export EDITOR=nvim
+export EDITOR=vi
 export VISUAL=nvim
 
-export BASH_COMPLETION_USER_FILE="$XDG_CONFIG_HOME"/bash/bash-completion
-export LESSHISTFILE="$XDG_CACHE_HOME"/less/history
-export LESSKEY="$XDG_CONFIG_HOME"/less/lesskey
-export CARGO_HOME="$XDG_DATA_HOME"/cargo
-export MYSQL_HISTFILE="$XDG_DATA_HOME"/mysql_history
-export NODE_REPL_HISTORY="$XDG_DATA_HOME"/node_repl_history
-export NVM_DIR="$XDG_DATA_HOME"/nvm
-export TIGRC_USER="$XDG_CONFIG_HOME"/tig/tigrc
-# TODO: prepare npmrc later on XDG Data Dir Specification
-export NPM_CONFIG_USERCONFIG=$XDG_CONFIG_HOME/npm/npmrc
-export GEM_HOME="$XDG_DATA_HOME"/gem
-export GEM_SPEC_CACHE="$XDG_CACHE_HOME"/gem
-export RUSTUP_HOME="$XDG_DATA_HOME"/rustup
-export RXVT_SOCKET="$XDG_RUNTIME_DIR"/urxvtd
-export XAUTHORITY="$XDG_RUNTIME_DIR"/Xauthority
-# xinit
-export XINITRC="$XDG_CONFIG_HOME"/X11/xinitrc
-export XSERVERRC="$XDG_CONFIG_HOME"/X11/xserverrc
-export WGETRC="$XDG_CONFIG_HOME/wgetrc"
-export SCREENRC="$XDG_CONFIG_HOME"/screen/screenrc
-export TMUX_TMPDIR="$XDG_RUNTIME_DIR"
-## ncurses
-export TERMINFO="$XDG_DATA_HOME"/terminfo
-export TERMINFO_DIRS="$XDG_DATA_HOME"/terminfo:/usr/share/terminfo
-if ! [ -d "$XDG_DATA_HOME"/wineprefixes ]; then
-	mkdir -p "$XDG_DATA_HOME"/wineprefixes
-fi
-export WINEPREFIX="$XDG_DATA_HOME"/wineprefixes/default
+export TERM=xterm-256color
