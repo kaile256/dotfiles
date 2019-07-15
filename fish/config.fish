2199a79302406fecb4bc48005a02a9042b7c00da
kaile256 <kaile256acc@gmail.com>
2019-07-15 18:34:39 +0900

    comment out related to linuxbrew

--- fish/config.fish
+++ fish/config.fish
@@ -4,22 +4,22 @@
 
 umask 022
 
-### VAR
-set -x EDITOR /home/linuxbrew/.linuxbrew/bin/nvim
-set -x VISUAL /home/linuxbrew/.linuxbrew/bin/nvim
-#set -x TMUX   /home/linuxbrew/.linuxbrew/bin/tmux
-set -x TERM screen-256color
-set -x WWW_HOME https://duckduckgo.com/
-
+#### VAR
+#set -x EDITOR /home/linuxbrew/.linuxbrew/bin/nvim
+#set -x VISUAL /home/linuxbrew/.linuxbrew/bin/nvim
+##set -x TMUX   /home/linuxbrew/.linuxbrew/bin/tmux
+#set -x TERM screen-256color
+#set -x WWW_HOME https://duckduckgo.com/
+#
 ### PATH
-set -g fish_user_paths "/home/linuxbrew/.linuxbrew/bin" $fish_user_paths
-set -g fish_user_paths "/home/linuxbrew/.linuxbrew/sbin" $fish_user_paths
-set -g fish_user_paths "/home/linuxbrew/.linuxbrew/bin/gem" $fish_user_paths
-set -g MANPATH /home/linuxbrew/.linuxbrew/share/man $MANPATH
-set -g INFOPATH /home/linuxbrew/.linuxbrew/share/info $INFOPATH
-set -gx HOMEBREW_TEMP ~/tmp
-set -g TMUX_TMPDIR ~/.cache/tmp
-
+#set -g fish_user_paths "/home/linuxbrew/.linuxbrew/bin" $fish_user_paths
+#set -g fish_user_paths "/home/linuxbrew/.linuxbrew/sbin" $fish_user_paths
+#set -g fish_user_paths "/home/linuxbrew/.linuxbrew/bin/gem" $fish_user_paths
+#set -g MANPATH /home/linuxbrew/.linuxbrew/share/man $MANPATH
+#set -g INFOPATH /home/linuxbrew/.linuxbrew/share/info $INFOPATH
+#set -gx HOMEBREW_TEMP ~/tmp
+#set -g TMUX_TMPDIR ~/.cache/tmp
+#
 ### Install fisher, the plugin manager
 if not functions -q fisher
     set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
