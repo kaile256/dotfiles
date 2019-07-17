f9c5005c71c249c6ed6ed515655c02841390d9ea
kaile256 <kaile256acc@gmail.com>
2019-07-17 16:13:06 +0900

    touch shared prompt for bash/zsh

---
+++ share/posix/prompt.sh
@@ -0,0 +1,7 @@
+## PROMPT_GIT; Plugin
+PROMPT_GIT="${XDG_CACHE_HOME}/bash/repos/github.com/magicmonty/bash-git-prompt/gitprompt.sh"
+if [ -f "${PROMPT_GIT}" ]; then
+  GIT_PROMPT_ONLY_IN_REPO=1
+  source "${PROMPT_GIT}"
+fi
+
