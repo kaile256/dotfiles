c81e60fdb9f350e5974d9e6454507162aa2d0983
kaile256 <kaile256acc@gmail.com>
2019-07-17 22:38:24 +0900

    mv env from bash's config

--- share/posix/env.sh
+++ share/posix/env.sh
@@ -1,4 +1,50 @@
-export EDITOR=vi
-export VISUAL=nvim
+#### ENVIROMENT VARIABLE
+# Private bin located as you like
+if [ -d "$HOME/bin" ] ; then
+    export PATH="$HOME/bin:$PATH"
+fi
 
+if [ -d "$HOME/.local/bin" ] ; then
+    export PATH="$HOME/.local/bin:$PATH"
+fi
+
+export HISTFILE="$XDG_DATA_HOME"/bash/history
 export TERM=xterm-256color
+## Vim/Neovim
+vi="$(which vi)"
+Nvim="$(which nvim)"
+if [ -f "${Nvim}" ]; then
+  export EDITOR="$Nvim"
+  export VISUAL="$Nvim"
+else
+  export EDITOR="$vi"
+  export VISUAL="$vi"
+fi
+
+
+## Tmux
+export TMUX="$(which tmux)"
+
+## For WSL
+#export DISPLAY="localhost:0.0"
+
+### Linuxbrew; for non-rolling releases.
+LINUXBREW="/home/linuxbrew/.linuxbrew"
+if [ -d ${LINUXBREW} ]; then
+  export PATH="${LINUXBREW}/bin:$PATH"
+  export PATH="${LINUXBREW}/sbin:$PATH"
+  eval $(${LINUXBREW}/bin/brew shellenv)
+#  APPS=(
+#  nvim
+#  fish
+#  tmux
+#  )
+#  for app in ${APPS[@]}; do
+#    app_path="${LINUXBREW}/bin/${app}"
+#    if [ -f ${app_path} ]; then
+#      export PATH="${app_path}:$PATH"
+#    fi
+#  done
+#
+fi
+
