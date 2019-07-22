4313d91948bfae8e9bfa6cfd201c9542996e5f70
kaile256 <kaile256acc@gmail.com>
2019-07-23 08:32:45 +0900

    At Termux, start up with kali!

--- fish/config.fish
+++ fish/config.fish
@@ -26,3 +26,8 @@ if not functions -q fisher
     curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
     fish -c fisher
 end
+
+if pwd | grep termux
+  cd
+  ./start-kali.sh
+end
