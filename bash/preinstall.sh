821df510b600502370984bac6b05ba4100551d3e
kaile256 <kaile256acc@gmail.com>
2019-07-24 01:32:13 +0900

    fin

--- bash/preinstall.sh
+++ bash/preinstall.sh
@@ -25,17 +25,11 @@ if ! [ -d ~/.cargo/bin ]; then
   echo "Done"
 fi
 
+### Dein -- vim
+PLUGIN_DIR=$HOME/.cache//nvim/dein
+INSTALL_DIR="${PLUGIN_DIR}/repos/github.com/Shougo/dein.vim"
 
-# make plugin dir and fetch dein
 if ! [ -e "$INSTALL_DIR" ]; then
-  ### Dein -- vim
-  PLUGIN_DIR=$HOME/.cache//nvim/dein
-  INSTALL_DIR="${PLUGIN_DIR}/repos/github.com/Shougo/dein.vim"
-  echo "Install to \"$INSTALL_DIR\"..."
-  if [ -e "$INSTALL_DIR" ]; then
-    echo "\"$INSTALL_DIR\" already exists!"
-  fi
-
   # check git command
   type git || {
     echo ""
