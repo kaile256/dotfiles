62469101b369e10447d70db3ec5afb8717c4f087
kaile256 <kaile256acc@gmail.com>
2019-08-29 12:15:40 +0900

    init

---
+++ emacs/.doom.d/config.el
@@ -0,0 +1,8 @@
+;;; .doom.d/config.el -*- lexical-binding: t; -*-
+
+;; Place your private configuration here
+
+(keyboard-translate ?\C-h ?\C-?)
+(global-set-key (kbd "C-?") 'help-for-help)
+
+;(create-fontset-from-ascii-font "SF Mono-12:weight=normal:slant=normal" nill "SF Mono-12")
