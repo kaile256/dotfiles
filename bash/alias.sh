85aad2686d314d33e3d3fc57b91ccfbae3d3bb89
kaile256 <kaile256acc@gmail.com>
2019-09-06 00:00:56 +0900

    New alias for fish.

--- bash/alias.sh
+++ bash/alias.sh
@@ -5,18 +5,20 @@
 
 set -Cu
 
-# Standard Command; ls {{{
+# Command; ls
 export 'LS_OPTION'='--color=auto'
 alias ls="ls --color=auto"
 alias la="ls --color=auto -A"
 alias ll="ls --color=auto -l"
 alias  l="ls --color=auto -lA"
-#}}}
 
-# Neovim
+# App; Shell
+alias f=fish
+# App; Neovim
 # `-s`: outputs no error even if no nvim server running.
 alias nvr="nvr -s"
 alias kvi="nvim -u $HOME/kaile256/dotfiles/nvim/init.vim"
-
-# w3m
+# App; w3m
 type w3m > /dev/null && alias w3m="w3m https://duckduckgo.com/?q="
+# App; Linux on Android
+alias pk='./pkali.sh'
