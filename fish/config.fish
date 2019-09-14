62a4352c2c0868dc1f4df00d86d29a97022c2045
kaile256 <kaile256acc@gmail.com>
2019-09-14 13:48:53 +0900

    On reconstruction.

--- fish/config.fish
+++ fish/config.fish
@@ -1,20 +1,20 @@
-#### Ref: .profile
-#### Ref: .zshenv
+# Ref: .profile
+# Ref: .zshenv
 
-## Never write "exec tmux" on script, or never access the WSL Distribution.
+# Never write "exec tmux" on script, or never access the WSL Distribution.
 source ~/.config/fish/myconf.d/*.fish
 source ~/.config/fish/myfuctions/*.fish
 
 umask 022
 
-#### VAR
+# VAR
 #set -x EDITOR /home/linuxbrew/.linuxbrew/bin/nvim
 #set -x VISUAL /home/linuxbrew/.linuxbrew/bin/nvim
-##set -x TMUX   /home/linuxbrew/.linuxbrew/bin/tmux
+#set -x TMUX   /home/linuxbrew/.linuxbrew/bin/tmux
 #set -x TERM screen-256color
 #set -x WWW_HOME https://duckduckgo.com/
-#
-### PATH
+
+# PATH
 #set -g fish_user_paths "/home/linuxbrew/.linuxbrew/bin" $fish_user_paths
 #set -g fish_user_paths "/home/linuxbrew/.linuxbrew/sbin" $fish_user_paths
 #set -g fish_user_paths "/home/linuxbrew/.linuxbrew/bin/gem" $fish_user_paths
@@ -22,16 +22,10 @@ umask 022
 #set -g INFOPATH /home/linuxbrew/.linuxbrew/share/info $INFOPATH
 #set -gx HOMEBREW_TEMP ~/tmp
 #set -g TMUX_TMPDIR ~/.cache/tmp
-#
-### Install fisher, the plugin manager
+
+# Install fisher, the plugin manager
 if not functions -q fisher
     set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
     curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
     fish -c fisher
 end
-
-# DOES'T work: no prompt, then <c-d> shows a prompt, but shows no user-input.
-#if pwd | grep termux; and test -x ~/pkali.sh
-#  cd
-#  ./pkali.sh
-#end
