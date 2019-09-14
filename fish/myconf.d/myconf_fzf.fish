3646b54ede6498cc5dc7993d76d3c0c86e6827de
kaile256 <kaile256acc@gmail.com>
2019-09-14 13:56:42 +0900

    Adapted ghq w/ fzf.

--- fish/myconf.d/myconf_fzf.fish
+++ fish/myconf.d/myconf_fzf.fish
@@ -6,3 +6,15 @@ set -x FZF_TMUX 1
 # use ripgrep; see hidden files, too. ignore .git and so on. ignore obj. in gitignore. follow symLinks.
 set -x FZF_DEFAULT_COMMAND 'rg --files --hidden --follow --glob "!.git/*"'
 
+## w/ ghq
+#function ghq_fzf_repo -d 'Repository search'
+#  ghq list --full-path | fzf --reverse --height=100% | read select
+#  [ -n "$select" ]; and cd "$select"
+#  echo " $select "
+#  commandline -f repaint
+#end
+#
+## keybind
+#function fish_user_key_bindings
+#  bind \cg ghq_fzf_repo
+#end
