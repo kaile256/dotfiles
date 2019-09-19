6d7664a4aa1c8bdb4dd781f22b4e6ffe4f1782a6
kaile256 <kaile256acc@gmail.com>
2019-09-19 13:44:03 +0900

    Unsuccessful

--- qtile/config.py
+++ qtile/config.py
@@ -26,7 +26,8 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-import os, subprocess
+import os
+import subprocess
 from libqtile.config import Key, Screen, Group, Drag, Click
 from libqtile.command import lazy
 from libqtile import layout, bar, widget
@@ -60,18 +61,29 @@ keys = [
     # Toggle between different layouts as defined below
     Key([mod], "Tab", lazy.next_layout()),
     Key([mod], "w", lazy.window.kill()),
+    Key([mod, "shift"], "q", lazy.shutdown()),
 
-    Key([mod, "control"], "r", lazy.restart()),
-    Key([mod, "control"], "q", lazy.shutdown()),
     # Layout hotkeys
     Key([mod], "h", lazy.layout.left()),
-    Key([mod], "l", lazy.layout.right()),
     Key([mod], "j", lazy.layout.down()),
     Key([mod], "k", lazy.layout.up()),
-    #Key([mod, "shift"], "j", lazy.layout.shuffle_down()),
-    #Key([mod, "shift"], "k", lazy.layout.shuffle_up()),
-    #Key([mod, "shift"], "n", lazy.layout.normalize()),
-    #Key([mod, "shift"], "m", lazy.layout.maximize()),
+    Key([mod], "l", lazy.layout.right()),
+
+    # Move windows up or down in current stack
+    Key([mod, "shift"], "k", lazy.layout.shuffle_up()),
+    Key([mod, "shift"], "j", lazy.layout.shuffle_down()),
+    Key([mod, "shift"], "h", lazy.layout.shuffle_left()),
+    Key([mod, "shift"], "l", lazy.layout.shuffle_right()),
+
+    # Grow windows up or down in current stack
+    Key([mod, "control"], "k", lazy.layout.grow_up()),
+    Key([mod, "control"], "j", lazy.layout.grow_down()),
+    Key([mod, "control"], "h", lazy.layout.grow_left()),
+    Key([mod, "control"], "l", lazy.layout.grow_right()),
+
+    # Normalize windows
+    # '=' causes an error to fail qtile.
+    #Key([mod], "0", lazy.layout.normalize()),
 
     # Spec hotkeys
     Key([mod, "shift"], "q", lazy.shutdown()),
@@ -81,7 +93,7 @@ keys = [
     Key([mod], "t", lazy.spawn("qterminal")),
 
     # Hotkey; Editor
-    Key([mod], "g", lazy.spawn("gnvim")),
+    #Key([mod], "g", lazy.spawn("gnvim")),
     Key([mod], "n", lazy.spawn("nvim-qt")),
     Key([mod], "e", lazy.spawn("emacs")),
     Key([mod], "c", lazy.spawn("code")),
@@ -90,65 +102,66 @@ keys = [
     Key([mod], "f", lazy.spawn("firefox")),
     Key([mod], "q", lazy.spawn("qutebrowser")),
 
-# System hotkeys
-        Key([mod, "shift", "control"], "F11", lazy.spawn("sudo hibernate-reboot")),
-        Key([mod, "shift", "control"], "F12", lazy.spawn("systemctl hibernate")),
-        Key([], "Print", lazy.spawn("scrot -e 'mv $f /home/user/screenshots/'")),
+    # System hotkeys
+    Key([mod, "shift", "control"], "F11",
+        lazy.spawn("sudo hibernate-reboot")),
+    Key([mod, "shift", "control"], "F12", lazy.spawn("systemctl hibernate")),
+    Key([], "Print", lazy.spawn("scrot -e 'mv $f /home/user/screenshots/'")),
 
-        # Media hotkeys
-        Key([], 'XF86AudioRaiseVolume', lazy.spawn('pulseaudio-ctl up 5')),
-        Key([], 'XF86AudioLowerVolume', lazy.spawn('pulseaudio-ctl down 5')),
-        Key([], 'XF86AudioMute', lazy.spawn('pulseaudio-ctl set 1')),
-        Key([mod], "r", lazy.spawncmd()),
+    # Media hotkeys
+    Key([], 'XF86AudioRaiseVolume', lazy.spawn('pulseaudio-ctl up 5')),
+    Key([], 'XF86AudioLowerVolume', lazy.spawn('pulseaudio-ctl down 5')),
+    Key([], 'XF86AudioMute', lazy.spawn('pulseaudio-ctl set 1')),
+    Key([mod], "r", lazy.spawncmd()),
 ]
 
 groups = [Group(i) for i in "1234567890"]
 
 for i in groups:
-  keys.extend([
-    # mod1 + letter of group = switch to group
-    Key([mod], i.name, lazy.group[i.name].toscreen()),
+    keys.extend([
+        # mod1 + letter of group = switch to group
+        Key([mod], i.name, lazy.group[i.name].toscreen()),
 
-    # mod1 + shift + letter of group = switch to & move focused window to group
-    Key([mod, "shift"], i.name, lazy.window.togroup(i.name)),
+        # mod1 + shift + letter of group = switch to & move focused window to group
+        Key([mod, "shift"], i.name, lazy.window.togroup(i.name)),
     ])
 
-  layouts = [
-      layout.Max(),
-      layout.Stack(num_stacks=2)
-      ]
+    layouts = [
+        layout.Max(),
+        layout.Stack(num_stacks=2)
+    ]
 
-  widget_defaults = dict(
-      font='Ricty Nerd Font',
-      fontsize=12,
-      padding=3,
-      )
-  extension_defaults = widget_defaults.copy()
+    widget_defaults = dict(
+        font='Ricty Nerd Font',
+        fontsize=12,
+        padding=3,
+    )
+    extension_defaults = widget_defaults.copy()
 
 screens = [
     Screen(
-      bottom=bar.Bar(
-        [
-          widget.GroupBox(),
-          widget.Prompt(),
-          widget.WindowName(),
-          widget.TextBox("default config", name="default"),
-          widget.Systray(),
-          widget.Clock(format='%Y-%m-%d %a %I:%M %p'),
-          ],
-        24,
+        bottom=bar.Bar(
+            [
+                widget.GroupBox(),
+                widget.Prompt(),
+                widget.WindowName(),
+                widget.TextBox("default config", name="default"),
+                widget.Systray(),
+                widget.Clock(format='%Y-%m-%d %a %I:%M %p'),
+            ],
+            24,
         ),
-      ),
-    ]
+    ),
+]
 
 # Drag floating layouts.
 mouse = [
     Drag([mod], "Button1", lazy.window.set_position_floating(),
-      start=lazy.window.get_position()),
+         start=lazy.window.get_position()),
     Drag([mod], "Button3", lazy.window.set_size_floating(),
-      start=lazy.window.get_size()),
+         start=lazy.window.get_size()),
     Click([mod], "Button2", lazy.window.bring_to_front())
-    ]
+]
 
 dgroups_key_binder = None
 dgroups_app_rules = []  # type: List
@@ -157,21 +170,21 @@ follow_mouse_focus = True
 bring_front_click = False
 cursor_warp = False
 floating_layout = layout.Floating(float_rules=[
-  {'wmclass': 'confirm'},
-  {'wmclass': 'dialog'},
-  {'wmclass': 'download'},
-  {'wmclass': 'error'},
-  {'wmclass': 'file_progress'},
-  {'wmclass': 'notification'},
-  {'wmclass': 'splash'},
-  {'wmclass': 'toolbar'},
-  {'wmclass': 'confirmreset'},  # gitk
-  {'wmclass': 'makebranch'},  # gitk
-  {'wmclass': 'maketag'},  # gitk
-  {'wname': 'branchdialog'},  # gitk
-  {'wname': 'pinentry'},  # GPG key password entry
-  {'wmclass': 'ssh-askpass'},  # ssh-askpass
-  ])
+    {'wmclass': 'confirm'},
+    {'wmclass': 'dialog'},
+    {'wmclass': 'download'},
+    {'wmclass': 'error'},
+    {'wmclass': 'file_progress'},
+    {'wmclass': 'notification'},
+    {'wmclass': 'splash'},
+    {'wmclass': 'toolbar'},
+    {'wmclass': 'confirmreset'},  # gitk
+    {'wmclass': 'makebranch'},  # gitk
+    {'wmclass': 'maketag'},  # gitk
+    {'wname': 'branchdialog'},  # gitk
+    {'wname': 'pinentry'},  # GPG key password entry
+    {'wmclass': 'ssh-askpass'},  # ssh-askpass
+])
 auto_fullscreen = True
 focus_on_window_activation = "smart"
 
