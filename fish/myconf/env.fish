ef6d6c24f3c4325c01f88de437495a686dfa4837
kaile256 <kaile256acc@gmail.com>
2019-10-04 16:19:29 +0900

    Added indexes.

--- fish/myconf/env.fish
+++ fish/myconf/env.fish
@@ -1,10 +1,23 @@
+#!/usr/bin/env fish
+
+# Language
 set -x DEVROOT "$HOME/dev"
-# GO
+
+# Go
 set -x GOPATH "$DEVROOT/go"
-set -x PATH "$GOPATH/bin" $PATH
 set -x GOENV_ROOT "$GOPATH/env/.goenv"
+## Ghq
+set -x GHQ_ROOT "$GOPATH/src"
 
 # Python
 set -x PYENV_ROOT "$DEVROOT/env/.pyenv"
-set -x PATH "$PYENV_ROOT/bin $PATH"
 source (pyenv init - | psub)
+
+# FZF
+set -x FZF_DEFAULT_COMMAND 'rg --files --hidden --follow --glob "!.git/*"'
+set -x FZF_LEGACY_KEYBINDINGS 1
+set -x FZF_PREVIEW_FILE_CMD "head -n 10"
+set -x FZF_PREVIEW_DIR_CMD "ls"
+## Tmux
+#set -x FZF_TMUX_HEIGHT "40%"
+#set -x FZF_DEFAULT_OPTS "--height $FZF_TMUX_HEIGHT"
