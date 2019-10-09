e9da8402cd9dd0e19b2f3780ea4e32769d08af58
kaile256 <kaile256acc@gmail.com>
2019-10-09 14:03:38 +0900

    Removed unnecessary features.

--- bash/alias.sh
+++ bash/alias.sh
@@ -3,8 +3,6 @@
 # From: .bashrc
 # Note: Write those alias for XDG Base Repository specification in 'env.sh'
 
-set -Cu
-
 # Command; ls
 export 'LS_OPTION'='--color=auto'
 alias ls="ls --color=auto"
@@ -17,6 +15,7 @@ alias ech='echo'
 
 # App; Shell
 alias f=fish
+
 # App; Neovim
 # `-s`: outputs no error even if no nvim server running.
 alias nvr="nvr -s"
@@ -24,20 +23,5 @@ alias kvi="nvim -u $HOME/kaile256/dotfiles/nvim/init.vim"
 # App; w3m
 type w3m > /dev/null && alias w3m="w3m https://duckduckgo.com/?q="
 # App; Linux on Android
-alias pk='./pkali.sh'
-
-# wget
-alias wget="wget --hsts-file=$XDG_CACHE_HOME/wget-hsts"
-### tmux
-alias tmux="tmux -f $XDG_CONFIG_HOME/tmux/tmux.conf"
-### dict
-alias dict="dict -c $XDG_CONFIG_HOME/dict/dictrc"
-### GnuPG
-alias gpg2="gpg2 --homedir $XDG_DATA_HOME/gnupg"
-### subversion
-alias svn="svn --config-dir $XDG_CONFIG_HOME/subversion"
-### weechat
-alias weechat="weechat -d $XDG_CONFIG_HOME/weechat"
-### xstart
-alias startx="startx $XDG_CONFIG_HOME/X11/xinitrc -- $XDG_CONFIG_HOME/X11/xserverrc vt1"
+alias pk='./start-kali.sh'
 
