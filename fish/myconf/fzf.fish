ad8d02cb75b79d1b8b89d06bd7734ef8157827dd
kaile256 <kaile256acc@gmail.com>
2019-10-16 20:25:53 +0900

    Modified fzf-mappings on fish for on neovim.

--- fish/myconf/fzf.fish
+++ fish/myconf/fzf.fish
@@ -1,9 +1,42 @@
-# Commands
+#!/usr/bin/env fish
 
 set -x FZF_LEGACY_KEYBINDINGS 0
 set -x FZF_TMUX 1
-# use ripgrep; see hidden files, too. ignore .git and so on. ignore obj. in gitignore. follow symLinks.
+# use ripgrep; ignore obj. in gitignore. follow symbolic links.
 set -x FZF_DEFAULT_COMMAND 'rg --files --hidden --follow --glob "!.git/*"'
+
+bind \ex '__fzf_find_file'
+# Mnemonic: Search
+bind \es '__fzf_reverse_isearch'
+#bind \ec '__fzf_cd'
+# Mnemonic: Cd
+bind \ec '__fzf_cd --hidden'
+#bind \eo '__fzf_open'
+# Mnemonic: Edit it
+bind \ee '__fzf_open --editor'
+
+if bind -M insert >/dev/null 2>/dev/null
+  bind -M insert \co '__fzf_find_file'
+  bind -M insert \es '__fzf_reverse_isearch'
+  #bind -M insert \ec '__fzf_cd'
+  bind -M insert \ec '__fzf_cd --hidden'
+  #bind -M insert \ev '__fzf_open'
+  bind -M insert \ev '__fzf_open --editor'
+end
+
+bind \cg '__ghq_repository_search'
+if bind -M insert >/dev/null 2>/dev/null
+  bind -M insert \cg '__ghq_repository_search'
+end
+
+#if set -q FZF_COMPLETE
+#  # seems same as find_file
+#  bind \t '__fzf_complete'
+#  if bind -M insert >/dev/null 2>/dev/null
+#    bind -M insert \t '__fzf_complete'
+#end
+#end
+
 
 ## w/ ghq
 #function ghq_fzf_repo -d 'Repository search'
