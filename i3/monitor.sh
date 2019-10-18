035b2794f6a257ad652f7cfa14c04b3ffae54f26
kaile256 <kaile256acc@gmail.com>
2019-10-18 11:31:29 +0900

    Made xrandr automatically display x on monitor above PC, though only at start-up.

--- i3/monitor.sh
+++ i3/monitor.sh
@@ -1,19 +1,31 @@
 #!/usr/bin/env bash
 
 # From: i3/config
-st_prev="disconnected"
+External="HDMI2"
 
-while :
-do
-  st_next=$(cat /sys/class/drm/card0-HDMI-A-1/status)
-  if [ $st_prev != $st_next ]; then
-    st_prev=$st_next
-    if [ $st_next = "connected" ]; then
-      xrandr --output eDP1 --mode 1600x900 --output HDMI1 --auto --above eDP1
-    else
-      xrandr --output eDP1 --mode 1600x900 --output HDMI1 --off
-    fi
-    ~/.fehbg
-  fi
-  sleep 1
-done
+xrandr | grep "$External connected" \
+    && xrandr --output eDP1 --auto \
+              --output "$External" --auto --above eDP1
+
+#st_prev="disconnected"
+#st_next=$(cat /sys/class/drm/card0-HDMI-A-1/status)
+#if [ $st_prev != $st_next ]; then
+#    st_prev=$st_next
+#    if [ $st_next = "connected" ]; then
+#        xrandr --output eDP1 --mode 1600x900 --output HDMI1 --auto --above eDP1
+#    else
+#        xrandr --output eDP1 --mode 1600x900 --output HDMI1 --off
+#    fi
+#    ~/.fehbg
+#fi
+#
+#st_next=$(cat /sys/class/drm/card0-HDMI-A-2/status)
+#if [ $st_prev != $st_next ]; then
+#    st_prev=$st_next
+#    if [ $st_next = "connected" ]; then
+#        xrandr --output eDP1 --auto --output HDMI2 --auto --above eDP1
+#    else
+#        xrandr --output eDP1 --mode 1600x900 --output HDMI2 --off
+#    fi
+#    ~/.fehbg
+#  fi
