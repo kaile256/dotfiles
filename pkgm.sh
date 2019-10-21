e8dbd64d390ebdee140cf7095fd5efac143e842b
kaile256 <kaile256acc@gmail.com>
2019-10-21 12:22:07 +0900

    Tried LINE on linux w/o google-chrome, but it turned out LINE corp. bans 3rd party clients.

--- pkgm.sh
+++ pkgm.sh
@@ -46,6 +46,7 @@ Package=(
   go
   googlecl  # google api for cli
   hub  # an official wrapper of git
+  #purple-line-git # WARNINIG: Line corp. bans people who uses 3rd party clients
   i3-volume
   #jinja2 # for neovim-qt
   luarocks
