5c7ea9d5ac7471effb69fa96a1a6b23eeb6e131b
kaile256 <kaile256acc@gmail.com>
2019-12-01 23:32:11 +0900

    Keep windows ratio after :Tagbar

--- .config/fish/myconf/alias.fish
+++ .config/fish/myconf/alias.fish
@@ -7,4 +7,13 @@ alias ech 'echo'
 alias pk '~/start-kail.sh'
 alias mirrors 'sudo pacman-mirrors -f && sudo pacman -Syy'
 alias rwatch 'cd ~/rustlings && rustlings watch'
-alias zg 'cd (ghq list --full-path | fzf)'
+
+# Note: fzf --bind to execute(cd {}) doesn't work; use 'accept' instead
+alias fzf-ghq "cd (ghq list --full-path | fzf --bind 'ctrl-v:execute(nvr {})')"
+alias gz fzf-ghq
+alias zg fzf-ghq
+
+# for functions
+alias zzh 'fzf-ssh'
+alias Qq 'fzf-pacman-Qq'
+alias qq 'fzf-pacman-Qq'
