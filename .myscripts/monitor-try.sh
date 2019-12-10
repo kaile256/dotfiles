bfa2fa16efcc567e50103133807782eb65870549
kaile256 <kaile256acc@gmail.com>
2019-12-11 00:23:51 +0900

    Wrote a script for monitor connection to toggle on i3-update

---
+++ .myscripts/monitor-try.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+# From: i3/config
+
+off=$(xrandr --query | grep " disconnected" | cut -d" " -f1)
+for m in $off; do
+  xrandr --output "$m" --off
+done
+
+#on=$(xrandr --query | grep " connected" | cut -d" " -f1 | grep HDMI)
+if monitor=$(xrandr --query | grep " connected" | cut -d" " -f1 | grep HDMI); then
+  xrandr --output eDP1 --auto
+  xrandr --output "$monitor" --auto --above eDP1
+else
+  xrandr --output eDP1 --auto
+fi
+
+#st_prev="disconnected"
+#st_next=$(cat /sys/class/drm/card0-HDMI-A-1/status)
+#if [ $st_prev != $st_next ]; then
+#    st_prev=$st_next
+#    if [ $st_next = "connected" ]; then
+#        xrandr --output eDP1 --mode 1600x900 --output HDMI1 --auto --above eDP1
+#    else
+#        xrandr --output eDP1 --mode 1600x900 --output HDMI1 --off
+#    fi
+#    ~/.fehbg
+#fi
+#
+#st_next=$(cat /sys/class/drm/card0-HDMI-A-2/status)
+#if [ $st_prev != $st_next ]; then
+#    st_prev=$st_next
+#    if [ $st_next = "connected" ]; then
+#        xrandr --output eDP1 --auto --output HDMI2 --auto --above eDP1
+#    else
+#        xrandr --output eDP1 --mode 1600x900 --output HDMI2 --off
+#    fi
+#    ~/.fehbg
+#  fi
