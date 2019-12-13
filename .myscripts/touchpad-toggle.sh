e929dab95ee7eb4247b85c81c84d3d52fafb076e
kaile256 <kaile256acc@gmail.com>
2019-12-13 18:09:00 +0900

    More precise notification on toggle touchpad

--- .myscripts/touchpad-toggle.sh
+++ .myscripts/touchpad-toggle.sh
@@ -2,8 +2,8 @@
 
 if xinput list Elan\ Touchpad | grep disabled >/dev/null ; then
   xinput enable Elan\ Touchpad
-  notify-send --expire-time 1200 'touchpad is now available'
+  notify-send --expire-time 1200 --urgency critical 'TouchPad: Activated'
 else
   xinput disable Elan\ Touchpad >/dev/null
-  notify-send --expire-time 1200 --urgency critical 'touchpad is disabled'
+  notify-send --expire-time 1200 'TouchPad: OFF'
 fi
