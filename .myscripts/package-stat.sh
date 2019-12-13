f389274f6d874df04f5172f5edfce7bd0dea7c2a
kaile256 <kaile256acc@gmail.com>
2019-12-13 20:10:41 +0900

    Bundled up a set of scripts for package-stat into a file as functions

---
+++ .myscripts/package-stat.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+
+package_check() {
+  if type pacman >/dev/null; then
+    if type yay >/dev/null; then
+      CHECKER='yay -Qu'
+    else
+      CHECKER='checkupdates'
+    fi
+
+    if ! updates=$($CHECKER 2> /dev/null | wc -l); then
+      updates=0
+    fi
+  fi
+
+  if [ "$updates" -gt 2 ]; then
+    echo " $updates"
+  else
+    echo " $updates"
+  fi
+}
+
+package_update() {
+  echo
+
+  if type pacman >/dev/null; then
+    if type yay >/dev/null ; then
+      echo 'Package: "yay -Syu"'
+      echo
+      yay -Syu
+    else
+      echo 'Package: "pacman -Syu"'
+      echo
+      sudo pacman -Syu
+    fi
+  fi
+
+  echo
+
+  read -r -p "Press enter to close this window..."
+}
+
+case "$1" in
+  --update)
+    package_update
+    ;;
+  *)
+    package_check
+    ;;
+esac
