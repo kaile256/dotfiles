d0d3f6fec5ed2e236df5d40b5e4702340724c98b
kaile256 <kaile256acc@gmail.com>
2019-12-22 16:56:36 +0900

    Config out

--- .myscripts/recording-toggle.sh
+++ .myscripts/recording-toggle.sh
@@ -1,10 +1,29 @@
 #!/usr/bin/env bash
 
 if pgrep screenkey || pgrep peek ; then
-  notify-send "End of Recording"
-  killall screenkey peek
+  killall --quiet screenkey peek
 else
-  notify-send "Let's start recording!"
-  screenkey -g 950x1000 -t 0.4 &
-  peek
+  notify-send --expire-time 1100 --urgency critical "Recording: Start"
+
+  # Note: 950 is about half of T490s screen; x1200 is about bottom
+  screenkey \
+    --opacity 0.3 \
+    --position bottom \
+    --geometry 950x1095 \
+    --font-size small \
+    --key-mode translated \
+    --timeout 0.4 \
+    &
+
+  peek --no-headerbar
+
+  while true; do
+    sleep 1
+    if ! pgrep peek >/dev/null; then
+      killall --quiet screenkey ffmpeg
+      notify-send --expire-time 1100 "Recording: END"
+      break
+    fi
+  done
 fi
+
