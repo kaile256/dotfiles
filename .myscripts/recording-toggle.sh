c1df671399c037320f513cbfe78e37cd5f2baaa2
kaile256 <kaile256acc@gmail.com>
2019-12-27 19:36:44 +0900

    Updated config for recording

--- .myscripts/recording-toggle.sh
+++ .myscripts/recording-toggle.sh
@@ -8,12 +8,17 @@ else
   notify-send --expire-time 1100 --urgency critical "Recording: Start"
 
   # Note: 950 is about half of T490s screen; x1200 is about bottom
+  # Web: https://gitlab.com/wavexx/screenkey#advanced-usage
+  # Note: press both shift/ctrl/alt to disable screenkey temporarily
+  # GUI Config: `$ screenkey --show-settings`
   screenkey \
-    --opacity 0.3 \
-    --position bottom \
+    --opacity 0.4 \
     --geometry 950x1095 \
+    --geometry 950x495 \
     --font-size small \
-    --key-mode translated \
+    --key-mode composed \
+    --vis-shift \
+    --mods-mode normal \
     --timeout 0.4 \
     &
 
