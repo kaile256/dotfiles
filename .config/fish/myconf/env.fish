86a7f17ad22fc8b1ede0e2924db5ee4e5f379eaa
kaile256 <kaile256acc@gmail.com>
2020-01-03 15:53:27 +0900

    Adjusted alignment

--- .config/fish/myconf/env.fish
+++ .config/fish/myconf/env.fish
@@ -2,8 +2,8 @@
 
 ## XDG Directory Specification
 set -x XDG_CONFIG_HOME $HOME/.config
-set -x XDG_CACHE_HOME $HOME/.cache
-set -x XDG_DATA_HOME $HOME/.local/share
+set -x XDG_CACHE_HOME  $HOME/.cache
+set -x XDG_DATA_HOME   $HOME/.local/share
 
 # Japanese IM
 set -x GTK_IM_MODULE fcitx
