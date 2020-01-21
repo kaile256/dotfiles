6ab03ebe7f24b7c381c28286f879f3dc55a35c78
kaile <ktigre3dec@gmail.com>
2020-01-21 13:17:45 +0900

    Added notes

--- .config/i3/lock.sh
+++ .config/i3/lock.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
 # Note: sleep 1 to suspend correctly
-# @ https://wiki.archlinux.jp/index.php/I3
-i3lock --nofork --image ~/Pictures/wallpaper/wall.png && sleep 1
+#   @ https://wiki.archlinux.jp/index.php/I3
+# mv wallpapers in /usr/share/backgrounds/
+
+# Man: i3lock(1)
+i3lock --ignore-empty-password --nofork --image /usr/share/backgrounds/wall.png && sleep 1
+
+#light-locker-command --lock
