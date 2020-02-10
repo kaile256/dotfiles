a244fb6458f857101a61a0a78466e318221bfa30
kaile <46470475+kaile256@users.noreply.github.com>
2020-02-10 10:38:27 +0900

    Update gem's path

--- .config/bash/path.sh
+++ .config/bash/path.sh
@@ -8,7 +8,7 @@ Path=(
   "$HOME/bin"
   "$CARGO_HOME/bin" # rust
   "$PYENV_ROOT/bin" # python
-  "$HOME/.gem/ruby/2.6.0/bin" # ruby
+  "$HOME/.gem/ruby/2.7.0/bin" # ruby
   "$GOPATH/bin" # go
   "$HOME/.node_modules/bin" # nodejs
   "$HOME/.yarn/bin" # nodejs
