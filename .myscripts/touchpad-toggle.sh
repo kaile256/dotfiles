c4e475f7060bc49bd46eddf6966f0d3fdebb0d49
kaile <46470475+kaile256@users.noreply.github.com>
2020-02-10 09:56:33 +0900

    Toggle TrackPoint addition to Touchpad in the same script

--- .myscripts/touchpad-toggle.sh
+++ .myscripts/touchpad-toggle.sh
@@ -6,6 +6,14 @@ if xinput list Elan\ Touchpad | grep disabled >/dev/null ; then
   xinput enable Elan\ Touchpad
   notify-send --expire-time 1200 --urgency critical 'TouchPad: Activated'
 else
-  xinput disable Elan\ Touchpad >/dev/null
+  xinput disable Elan\ Touchpad >/dev/null 2>&1
   notify-send --expire-time 1200 'TouchPad: OFF'
+fi
+
+if xinput list Elan\ TrackPoint | grep disabled >/dev/null ; then
+  xinput enable Elan\ TrackPoint
+  notify-send --expire-time 1200 --urgency critical 'TrackPoint: Activated'
+#else
+#  xinput disable Elan\ TrackPoint >/dev/null 2>&1
+#  notify-send --expire-time 1200 'TrackPoint: OFF'
 fi
