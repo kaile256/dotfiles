79ee623c3453694b3f358149fda7edf82e9db0b8
kaile256 <46470475+kaile256@users.noreply.github.com>
2020-03-12 09:40:17 +0900

    Substitue "zplugin" with "zinit"

--- .config/zsh/zinit.zsh
+++ .config/zsh/zinit.zsh
@@ -1,49 +1,50 @@
-#### Install Zplugin if have not ####
-# Ref: https://github.com/zdharma/zplugin#installation
+# Install zinit if have not
+# Ref: https://github.com/zdharma/zinit#installation
 # WARNING: no use to install the other directory
-. "${XDG_CONFIG_HOME}/zplugin/bin/zplugin.zsh"
-type zplugin >> /dev/null || sh -c "$(curl -fsSL https://raw.githubusercontent.com/zdharma/zplugin/master/doc/mod-install.sh)"
+. "${XDG_CONFIG_HOME}/zinit/bin/zinit.zsh"
 
-declare -A ZPLGM
-ZPLGM=(
-  ['BIN_DIR']="$HOME/.config/zplugin/bin"
-  ['HOME_DIR']="$HOME/.config/zplugin"
-  ['PLUGINS_DIR']="$HOME/.config/zplugin/plugins"
-  ['COMPLETIONS_DIR']="$HOME/.config/zplugin/completions"
-  ['SNIPPETS_DIR']="$HOME/.config/zplugin/snippets"
-  ['ZCOMPDUMP_PATH']="$HOME/.config/zplugin"
-  ['COMPINIT_OPTS']="$HOME/.config/zplugin"
-  ['MUTE_WARNINGS']=0
-)
+type zinit >/dev/null 2>&1 || sh -c "$(curl -fsSL https://raw.githubusercontent.com/zdharma/zinit/master/doc/install.sh)"
 
-autoload -Uz _zplugin
-(( ${+_comps} )) && _comps[zplugin]=_zplugin
+#declare -A ZPLGM
+#ZPLGM=(
+#  ['BIN_DIR']="$HOME/.config/zinit/bin"
+#  ['HOME_DIR']="$HOME/.config/zinit"
+#  ['PLUGINS_DIR']="$HOME/.config/zinit/plugins"
+#  ['COMPLETIONS_DIR']="$HOME/.config/zinit/completions"
+#  ['SNIPPETS_DIR']="$HOME/.config/zinit/snippets"
+#  ['ZCOMPDUMP_PATH']="$HOME/.config/zinit"
+#  ['COMPINIT_OPTS']="$HOME/.config/zinit"
+#  ['MUTE_WARNINGS']=0
+#)
+
+autoload -Uz _zinit
+(( ${+_comps} )) && _comps[zinit]=_zinit
 #### End of Zplugin's installer chunk ####
 
-zplugin light "mafredri/zsh-async"
+zinit light "mafredri/zsh-async"
 # Prompt Theme
 # good on vim-term to two-row prompt to yank.
-#zplugin ice pick"async.zsh" src"pure.zsh"; zplugin light sindresorhus/pure
-zplugin ice rc"polyglot.plugin.zsh"; zplugin light "agkozak/polyglot"
-#zplugin ice src"spaceship.zsh"; zplugin light denysdovhan/spaceship-prompt
+#zinit ice pick"async.zsh" src"pure.zsh"; zinit light sindresorhus/pure
+zinit ice rc"polyglot.plugin.zsh"; zinit light "agkozak/polyglot"
+#zinit ice src"spaceship.zsh"; zinit light denysdovhan/spaceship-prompt
 
-zplugin ice wait"!0" atload"_zsh_autosuggest_start"
-zplugin light "zsh-users/zsh-autosuggestions"
-zplugin light "zsh-users/zsh-completions"
-zplugin light "zdharma/fast-syntax-highlighting"
+zinit ice wait"!0" atload"_zsh_autosuggest_start"
+zinit light "zsh-users/zsh-autosuggestions"
+zinit light "zsh-users/zsh-completions"
+zinit light "zdharma/fast-syntax-highlighting"
 
 # FZF
-zplugin ice from"gh-r" as"program"; zplugin load "junegunn/fzf-bin"
+zinit ice from"gh-r" as"program"; zinit load "junegunn/fzf-bin"
 # LS COLOR
-zplugin ice atclone"dircolors -b LS_COLORS > c.zsh" atpull'%atclone' pick"c.zsh" nocompile'!'
-zplugin light "trapd00r/LS_COLORS"
+zinit ice atclone"dircolors -b LS_COLORS > c.zsh" atpull'%atclone' pick"c.zsh" nocompile'!'
+zinit light "trapd00r/LS_COLORS"
 
-zplugin ice wait'!0'; zplugin light "vintersnow/anyframe"
-zplugin ice wait'!0'; zplugin light "b4b4r07/enhancd"
-zplugin ice wait'!0'; zplugin light "lukechilds/zsh-nvm"
-##zplugin ice wait'!0'; zplugin light "greymd/tmux-xpanes"
+zinit ice wait'!0'; zinit light "vintersnow/anyframe"
+zinit ice wait'!0'; zinit light "b4b4r07/enhancd"
+zinit ice wait'!0'; zinit light "lukechilds/zsh-nvm"
+##zinit ice wait'!0'; zinit light "greymd/tmux-xpanes"
 #
-zplugin ice wait"!0" atinit"zpcompinit; zpcdreplay"
-zplugin light "zsh-users/zsh-syntax-highlighting"
+zinit ice wait"!0" atinit"zpcompinit; zpcdreplay"
+zinit light "zsh-users/zsh-syntax-highlighting"
 
 
