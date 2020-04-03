d5f1791c3f65ed572ac758b8863fb443f4c1d93d
kaile256 <46470475+kaile256@users.noreply.github.com>
2020-04-03 10:15:26 +0900

    Replace alias.zsh with a symbolic link to bash's

--- .config/zsh/alias.zsh
+++ .config/zsh/alias.zsh
@@ -1,23 +1,36 @@
-#!/usr/bin/env zsh
+#!/usr/bin/env bash
 
 # From: .bashrc
-# Note: Write those alias for XDG Base Repository specification in 'env.sh'
+# Ref: fish/myconf/alias.fish
 
-# Standard Command; ls {{{
+# Note: write XDG-related aliases in 'env.sh'
+
+# Command; ls
 export 'LS_OPTION'='--color=auto'
 alias ls="ls --color=auto"
 alias la="ls --color=auto -A"
 alias ll="ls --color=auto -l"
 alias  l="ls --color=auto -lA"
+# Command; echo
 alias ec='echo'
 alias ech='echo'
-alias f='fish'
-#}}}
 
-# Neovim
+# App; Shell
+alias f=fish
+
+# App; Neovim
 # `-s`: outputs no error even if no nvim server running.
 alias nvr="nvr -s"
 alias kvi="nvim -u $HOME/kaile256/dotfiles/nvim/init.vim"
 
-# w3m
-type w3m > /dev/null && alias w3m="w3m https://duckduckgo.com/?q="
+alias pg='pgrep'
+
+# App; Linux on Android
+alias pk='~/start-kali.sh'
+
+# TODO: accept arguments to restrict candidates
+alias fzf-fonts='fc-list | fzf'
+
+# Ref: https://qiita.com/yutakatay/items/f19b2a0a962a587db5cf
+#alias nvim-how-slower=echo "scale=3; $(nvim --startuptime /tmp/stime_mine.log -c 'quit' > /dev/null && tail -n 1 /tmp/stime_mine.log | cut -d ' ' -f1) / $(nvim -u DEFAULTS --startuptime /tmp/stime_def.log -c 'quit' > /dev/null && tail -n 1 /tmp/stime_def.log | cut -d ' ' -f1)" | bc | xargs -i echo {}x slower your Vim than the default.
+#alias vim-how-slower=echo "scale=3; $(vim --startuptime /tmp/stime_mine.log -c 'quit' > /dev/null && tail -n 1 /tmp/stime_mine.log | cut -d ' ' -f1) / $(vim -u DEFAULTS --startuptime /tmp/stime_def.log -c 'quit' > /dev/null && tail -n 1 /tmp/stime_def.log | cut -d ' ' -f1)" | bc | xargs -i echo {}x slower your Vim than the default.
