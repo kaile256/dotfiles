dc7a78494da96be1a4b80f93daa7071da718b4f2
kaile256 <46470475+kaile256@users.noreply.github.com>
2020-04-28 00:56:28 +0900

    Remove unnecessary fold end

--- .config/bash/xdg.sh
+++ .config/bash/xdg.sh
@@ -85,5 +85,3 @@ export INPUTRC="$XDG_CONFIG_HOME/readline/inputrc"
 export NODE_REPL_HISTORY="$XDG_DATA_HOME/node_repl_history"
 ## npm
 export NPM_CONFIG_USERCONFIG="$XDG_CONFIG_HOME/npm/npmrc"
-#}}}
-
