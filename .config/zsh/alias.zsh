7ae33aa38df458a8e8de41fb285dfd246e44101c
kaile256 <46470475+kaile256@users.noreply.github.com>
2020-05-23 21:53:09 +0900

    Update alias

--- .config/zsh/alias.zsh
+++ .config/zsh/alias.zsh
@@ -6,11 +6,11 @@
 # Note: write XDG-related aliases in 'env.sh'
 
 # Command; ls
-export 'LS_OPTION'='--color=auto'
-alias ls="ls --color=auto"
-alias la="ls --color=auto -A"
-alias ll="ls --color=auto -l"
-alias  l="ls --color=auto -lA"
+export LS_OPTION="--color=auto"
+alias ls='ls $LS_OPTION'
+alias la='ls $LS_OPTION -A'
+alias ll='ls $LS_OPTION -l'
+alias  l='ls $LS_OPTION -lA'
 # Command; echo
 alias ec='echo'
 alias ech='echo'
@@ -20,8 +20,8 @@ alias f=fish
 
 # App; Neovim
 # `-s`: outputs no error even if no nvim server running.
-alias nvr="nvr -s"
-alias kvi="nvim -u $HOME/kaile256/dotfiles/nvim/init.vim"
+alias nvr="nvr -s --remote-wait-silent"
+alias kvi='nvim -u $HOME/kaile256/dotfiles/nvim/init.vim'
 
 alias pg='pgrep'
 
