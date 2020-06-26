70212620ead69a73c9577996470600bcb5f8434d
kaile256 <46470475+kaile256@users.noreply.github.com>
2020-06-26 22:11:31 +0900

    feat(sonictemplate) Update for atcoder

--- .config/nvim/data/sonictemplate/cpp/base-atcoder.cpp
+++ .config/nvim/data/sonictemplate/cpp/base-atcoder.cpp
@@ -1,11 +1,11 @@
 #include <algorithm>
+#include <cstdio>
 #include <iostream>
 #include <string>
 #include <vector>
-#include <cstdio>
 
-#define REP(i, A, Z) for (auto i = (A); (i) < (Z); ++(i))
-#define RREP(i, A, Z) for (auto i = (A); (i) > (Z); --(i))
+#define REP(i, A, Z) for (auto i = (A); i < (Z); ++i)
+#define RREP(i, A, Z) for (auto i = (A); i > (Z); --i)
 #define rep(i, n) REP(i, 0, (n))
 #define rep1(i, n) REP(i, 1, (n))
 #define rrep(i, n) RREP(i, (n), 0)
@@ -14,19 +14,22 @@
 #define ALL(x) (x).begin(), (x).end()
 #define RALL(x) (x).rbegin(), (x).rend()
 
-#define optimize_cin() ios::sync_with_stdio(!cin.tie(0))
+#define optimize_cin() std::ios::sync_with_stdio(!cin.tie(0))
 #define pb push_back
 #define endl '\n'
 
 constexpr double MOD = 1e+9 + 7;
 constexpr double EPS = 1e-12;
-constexpr double PI  = 3.14159265358979323846;
+constexpr double PI = 3.14159265358979323846;
 
 using ll = long long;
 
-using namespace std;
+using std::cin;
+using std::cout;
+using std::iterator;
+using std::vector;
 
-template <typename T> inline void cout_vector(vector<T> &v) {
+template <typename T> inline void answer(vector<T> &v) {
   for (auto &&x : v)
     cout << x << ' ';
   cout << '\n';
@@ -48,14 +51,9 @@ template <class T> inline bool chmax(T &a, T b) {
   return false;
 }
 
-template <typename T> inline bool answer() {
-
-  cout << endl;
-}
-
 int main() {
   cin.tie(0);
-  ios::sync_with_stdio(false);
+  std::ios::sync_with_stdio(false);
+
   {{_cursor_}}
 }
-
