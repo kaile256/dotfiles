53169f8496a1c444ee9b78f83af3f0eece9541cf
kaile256 <46470475+kaile256@users.noreply.github.com>
2020-07-12 19:34:28 +0900

    feat(keybaord) notify when connected

--- .myscripts/connect-keyboard.sh
+++ .myscripts/connect-keyboard.sh
@@ -4,8 +4,26 @@ set -Ceu
 
 pgrep bluetoothd || exit
 
+bluetoothctl power on
+
 KEYBOARD_NAME='TrackPoint Keyboard'
 MAC_KEYBOARD=$(bluetoothctl paired-devices | grep -i "$KEYBOARD_NAME" | awk '{print $2}')
+has_disconnected=$(bluetoothctl info "$MAC_KEYBOARD" | grep 'Connected: no' \
+  && echo -n 'true')
 
-bluetoothctl power on
 bluetoothctl connect "$MAC_KEYBOARD"
+
+if $has_disconnected ; then
+  notify_msg() {
+    msg=$1
+    if type notify-send >/dev/null 2>&1; then
+      notify-send \
+        --expire-time 2000 \
+        --urgency=critical \
+        "$msg"
+    fi
+    echo "$msg"
+  }
+
+  bluetoothctl info "$MAC_KEYBOARD" && notify_msg "$KEYBOARD_NAME is connected"
+fi
