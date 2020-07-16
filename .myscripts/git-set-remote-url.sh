a5ea98c1e346730c1da009b24edc5ec892dd5a44
kaile256 <46470475+kaile256@users.noreply.github.com>
2020-07-16 11:43:51 +0900

    feat(git) rename user for url-alias with no arg

--- .myscripts/git-set-remote-url.sh
+++ .myscripts/git-set-remote-url.sh
@@ -4,10 +4,13 @@ set -Ceu
 
 git fetch --unshallow >/dev/null 2>&1 || true
 
+ORIG=$(git config --get remote.origin.url)
+ALIAS=origin
 USER=kaile256
 
-if [ $# == 1 ]; then
-  ALIAS=origin
+if [ $# == 0 ]; then
+  URL=$(git rev-parse --show-toplevel | sed -e 's;.*/;;')
+elif [ $# == 1 ]; then
   URL=$1
 elif [ $# == 2 ]; then
   ALIAS=$1
@@ -22,4 +25,10 @@ echo "$URL" | grep -e ".\+/.\+" >/dev/null 2>&1 \
 echo "$URL" | grep 'https://github.com/' >/dev/null 2>&1 \
   || URL="https://github.com/$URL"
 
-git remote set-url "$ALIAS" "$URL"
+if git config --get remote.origin.url | grep "$URL" >/dev/null 2>&1; then
+  echo "\"$ALIAS\" has already set to \"$URL\""
+  exit
+fi
+
+git remote set-url "$ALIAS" "$URL" \
+  && echo -e "Done! \"$ALIAS\" is updated:\n old: \"$ORIG\"\n new: \"$URL\""
