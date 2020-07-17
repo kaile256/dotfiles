fbdc70b376112454ce9be3cee210d0a028c9fa3c
kaile256 <46470475+kaile256@users.noreply.github.com>
2020-07-17 17:55:59 +0900

    feat(lock) change the wallpaper

--- .config/i3/lock.sh
+++ .config/i3/lock.sh
@@ -4,7 +4,9 @@
 #   @ https://wiki.archlinux.jp/index.php/I3
 # mv wallpapers in /usr/share/backgrounds/
 
+# Note: the image must be a png format.
+IMAGE="$MY_MEMO/data/wallpaper/Zion-National-Park-Springdale-Utah-USA.png"
 # Man: i3lock(1)
-i3lock --ignore-empty-password --nofork --image /usr/share/backgrounds/wall.png && sleep 1
+i3lock --ignore-empty-password --nofork --image "$IMAGE" && sleep 1
 
 #light-locker-command --lock
