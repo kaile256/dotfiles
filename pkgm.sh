7ae8ae32dfa57a3a4bf781d93a726b3f65f9ff20
kaile256 <46470475+kaile256@users.noreply.github.com>
2020-08-13 17:30:13 +0900

    feat(pkg) install universal ctags

--- pkgm.sh
+++ pkgm.sh
@@ -28,6 +28,7 @@ PACKAGEs=(
   xorg-xinput # removed with `bmenu`
   lshw # removed with `bmenu`
 
+  universal-ctags-git # esp. for Vista on Vim
   python-pyperclip # code_select.py on qutebrowser
   xbindkeys # to disable middle to paste, but enable scroll.
   otf-nerd-fonts-fira-code
