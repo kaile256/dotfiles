cf052ab780e5f5f4acc568ed9d52834182bd0f19
kaile256 <46470475+kaile256@users.noreply.github.com>
2020-11-06 11:12:39 +0900

    feat(qute): exclude smaller monitor from darkmode

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -37,7 +37,7 @@ c.hints.selectors["code"] = [
 def should_be_darkmode():
     monitor_sizes_to_darkmode = [
         '1040mm x 590mm',
-        '530mm x 300mm',
+        # '530mm x 300mm',
     ]
 
     possible_monitors = subprocess.check_output(['xrandr'], text=True)
