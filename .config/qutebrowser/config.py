c6583969b53f65aa6efef0b754917526772825d1
kaile256 <46470475+kaile256@users.noreply.github.com>
2020-11-09 18:59:57 +0900

    feat(qute): set theme derived from "nord"

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -56,3 +56,138 @@ SHOULD_BE_DARKMODE = should_be_darkmode()
 c.colors.webpage.darkmode.enabled = SHOULD_BE_DARKMODE
 # c.colors.webpage.darkmode.grayscale.all = SHOULD_BE_DARKMODE
 # c.colors.webpage.prefers_color_scheme_dark = SHOULD_BE_DARKMODE
+
+THEME = 'nord'
+# base16-qutebrowser (https://github.com/theova/base16-qutebrowser)
+# Base16 qutebrowser template by theova
+# - Tried themes: (capitalize to light themes; bracket to worth modifying)
+#   - too ambiguous to distinguish focused tab in short time:
+#       [harmonic-dark], [nord], monokai, chalk, dracula, zenburn, tomorrow-night,
+#       spacemacs, espresso, railscasts, onedark, Gruvbox-light-hard,
+#       gruvbox-dark-medium, gruvbox-dark-pale, Harmonic-light,
+#       gruvbox-dark-soft, material, mackintosh
+#   - too colorful to read: tango, brogrammer, papercolor-dark, apathy,
+#       material-darker
+#   - too dim/low contrast to read words:
+#       solarized-dark, Solarized-light, material-vivid
+
+if THEME == 'nord':
+    # Nord scheme by arcticicestudio (modified)
+
+    # Increment to make it darker.
+    BLACK_2 = "#2E3440"
+    BLACK_1 = "#3B4252"
+    BLACK_0 = "#434C5E"
+    GREY = "#4C566A"
+    WHITE_2 = "#D8DEE9"
+    WHITE_1 = "#E5E9F0"
+    WHITE_0 = "#ECEFF4"
+
+    RED = "#BF616A"
+    ORANGE = "#D08770"
+    YELLOW = "#EBCB8B"
+    base07 = "#8FBCBB"
+    GREEN = "#A3BE8C"
+    CYAN = "#88C0D0"
+    base0D = "#81A1C1"
+    PURPLE = "#B48EAD"
+    BLUE = "#5E81AC"
+
+    c.colors.completion.fg = WHITE_1
+    c.colors.completion.odd.bg = BLACK_1
+    c.colors.completion.even.bg = BLACK_2
+    c.colors.completion.category.fg = YELLOW
+    c.colors.completion.category.bg = BLACK_2
+    c.colors.completion.category.border.top = BLACK_2
+    c.colors.completion.category.border.bottom = BLACK_2
+
+    c.colors.completion.item.selected.fg = WHITE_1
+    c.colors.completion.item.selected.bg = BLACK_0
+    c.colors.completion.item.selected.border.top = BLACK_0
+    c.colors.completion.item.selected.border.bottom = BLACK_0
+    c.colors.completion.item.selected.match.fg = GREEN
+    c.colors.completion.match.fg = GREEN
+    c.colors.completion.scrollbar.fg = WHITE_1
+    c.colors.completion.scrollbar.bg = BLACK_2
+
+    c.colors.contextmenu.disabled.bg = BLACK_1
+    c.colors.contextmenu.disabled.fg = WHITE_2
+    c.colors.contextmenu.menu.bg = BLACK_2
+    c.colors.contextmenu.menu.fg = WHITE_1
+    c.colors.contextmenu.selected.bg = BLACK_0
+    c.colors.contextmenu.selected.fg = WHITE_1
+
+    c.colors.downloads.bar.bg = BLACK_2
+    c.colors.downloads.start.fg = BLACK_2
+    c.colors.downloads.start.bg = base0D
+    c.colors.downloads.stop.fg = BLACK_2
+    c.colors.downloads.stop.bg = CYAN
+    c.colors.downloads.error.fg = RED
+
+    c.colors.hints.fg = BLACK_2
+    c.colors.hints.bg = YELLOW
+    c.colors.hints.match.fg = WHITE_1
+    c.colors.keyhint.fg = WHITE_1
+    c.colors.keyhint.suffix.fg = WHITE_1
+    c.colors.keyhint.bg = BLACK_2
+
+    c.colors.messages.error.fg = BLACK_2
+    c.colors.messages.error.bg = RED
+    c.colors.messages.error.border = RED
+    c.colors.messages.warning.fg = BLACK_2
+    c.colors.messages.warning.bg = PURPLE
+    c.colors.messages.warning.border = PURPLE
+    c.colors.messages.info.fg = WHITE_1
+    c.colors.messages.info.bg = BLACK_2
+    c.colors.messages.info.border = BLACK_2
+
+    c.colors.prompts.fg = WHITE_1
+    c.colors.prompts.border = BLACK_2
+    c.colors.prompts.bg = BLACK_2
+    c.colors.prompts.selected.bg = BLACK_0
+    c.colors.statusbar.normal.fg = GREEN
+    c.colors.statusbar.normal.bg = BLACK_2
+    c.colors.statusbar.insert.fg = BLACK_2
+    c.colors.statusbar.insert.bg = base0D
+    c.colors.statusbar.passthrough.fg = BLACK_2
+    c.colors.statusbar.passthrough.bg = CYAN
+    c.colors.statusbar.private.fg = BLACK_2
+    c.colors.statusbar.private.bg = BLACK_1
+    c.colors.statusbar.command.fg = WHITE_1
+    c.colors.statusbar.command.bg = BLACK_2
+    c.colors.statusbar.command.private.fg = WHITE_1
+    c.colors.statusbar.command.private.bg = BLACK_2
+    c.colors.statusbar.caret.fg = BLACK_2
+    c.colors.statusbar.caret.bg = PURPLE
+    c.colors.statusbar.caret.selection.fg = BLACK_2
+    c.colors.statusbar.caret.selection.bg = base0D
+    c.colors.statusbar.progress.bg = base0D
+    c.colors.statusbar.url.fg = WHITE_1
+    c.colors.statusbar.url.error.fg = RED
+    c.colors.statusbar.url.hover.fg = WHITE_1
+    c.colors.statusbar.url.success.http.fg = CYAN
+    c.colors.statusbar.url.success.https.fg = GREEN
+    c.colors.statusbar.url.warn.fg = PURPLE
+
+    c.colors.tabs.bar.bg = BLACK_2
+    c.colors.tabs.indicator.start = base0D
+    c.colors.tabs.indicator.stop = CYAN
+    c.colors.tabs.indicator.error = RED
+
+    c.colors.tabs.odd.fg = BLACK_1
+    c.colors.tabs.even.fg = BLACK_1
+    c.colors.tabs.selected.odd.fg = WHITE_1
+    c.colors.tabs.selected.even.fg = WHITE_1
+    c.colors.tabs.pinned.selected.odd.fg = WHITE_1
+    c.colors.tabs.pinned.selected.even.fg = WHITE_1
+    c.colors.tabs.pinned.odd.fg = BLACK_1
+    c.colors.tabs.pinned.even.fg = BLACK_1
+
+    c.colors.tabs.odd.bg = WHITE_2
+    c.colors.tabs.even.bg = WHITE_2
+    c.colors.tabs.pinned.odd.bg = ORANGE
+    c.colors.tabs.pinned.even.bg = ORANGE
+    c.colors.tabs.selected.odd.bg = BLACK_2
+    c.colors.tabs.selected.even.bg = BLACK_2
+    c.colors.tabs.pinned.selected.odd.bg = BLACK_2
+    c.colors.tabs.pinned.selected.even.bg = BLACK_2
