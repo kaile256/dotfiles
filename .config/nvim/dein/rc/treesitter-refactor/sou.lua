2f2d90d0cd71ae1c1ada2cac52abd04fc6e7972b
kaile256 <46470475+kaile256@users.noreply.github.com>
2021-01-14 14:32:25 +0900

    feat(ts-refactor): add mappings to navigation

--- .config/nvim/dein/rc/treesitter-refactor/sou.lua
+++ .config/nvim/dein/rc/treesitter-refactor/sou.lua
@@ -20,16 +20,17 @@ require'nvim-treesitter.configs'.setup {
       },
     },
 
-    -- navigation = {
-    --   enable = true,
-    --   keymaps = {
-    --     goto_definition = "gd",
-    --     list_definitions = "gD",
-    --     list_definitions_toc = "gO",
-    --     goto_next_usage = ']u',
-    --     goto_previous_usage = "[u",
-    --   },
-    -- },
+    navigation = {
+      enable = true,
+      keymaps = {
+        goto_definition = "<Plug>(ts-refactor-goto-definition)",
+        list_definitions = "<Plug>(ts-refactor-list-definition)",
+        list_definitions_toc = "<Plug>(ts-refactor-toc-definition)",
+
+        goto_next_usage = "]u",
+        goto_previous_usage = "[u",
+      },
+    },
 
   },
 }
