880e963543e607e958a56c717a39bfae34434fbd
kaile256 <46470475+kaile256@users.noreply.github.com>
2021-02-25 11:00:36 +0900

    refactor(hlslens): improve readability of the func

--- .config/nvim/dein/rc/hlslens/pos.lua
+++ .config/nvim/dein/rc/hlslens/pos.lua
@@ -1,6 +1,8 @@
 -- TOML: appearance.toml
 -- Repo: kevinhwang91/nvim-hlslens
 
+local vim = vim
+
 require('hlslens').setup({
   -- Enable hlslens on searching
   auto_enable = true,
@@ -14,33 +16,35 @@ require('hlslens').setup({
     local indicator, text, chunks
     local a_r_idx = math.abs(r_idx)
 
+    local hl_group = 'HlSearchLens'
     local ind_above = '▲'
     local ind_below = '▼'
 
     if a_r_idx > 1 then
-      indicator = string.format('%d%s', a_r_idx,
-      sfw ~= (r_idx > 1) and ind_above or ind_below)
+      indicator = string.format('%d%s', a_r_idx, sfw == (r_idx > 1) and ind_below or ind_above)
     elseif a_r_idx == 1 then
-      indicator = sfw ~= (r_idx == 1) and ind_above or ind_below
+      indicator = sfw == (r_idx == 1) and ind_below or ind_above
     else
       indicator = ''
     end
 
     if loc ~= 'c' then
       text = string.format('[%s %d]', indicator, idx)
-      chunks = {{' ', 'Ignore'}, {text, 'HlSearchLens'}}
+    elseif indicator ~= '' then
+      -- For the other matches around.
+      text = string.format('[%s %d/%d]', indicator, idx, count)
     else
-      if indicator ~= '' then
-        text = string.format('[%s %d/%d]', indicator, idx, count)
-      else
-        text = string.format('[%d/%d]', idx, count)
-      end
-      chunks = {
-        {' ', 'Ignore'},
-        {text, 'HlSearchLensCur'},
-      }
-      vim.api.nvim_buf_clear_namespace(0, namespace, lnum - 1, lnum)
+      -- For the match the very beginning of which cursor attaches.
+      text = string.format('[%d/%d]', idx, count)
+      hl_group = 'HlSearchLensCur'
     end
+
+    vim.api.nvim_buf_clear_namespace(0, namespace, lnum - 1, lnum)
+
+    text = ' ' .. text
+    chunks = {
+      { text, hl_group },
+    }
     vim.api.nvim_buf_set_virtual_text(0, namespace, lnum - 1, chunks, {})
-  end
+  end;
 })
