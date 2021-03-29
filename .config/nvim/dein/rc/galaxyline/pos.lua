2d7ef004f6695d24cf8bef0371a5511b0c815636
kaile256 <46470475+kaile256@users.noreply.github.com>
2021-03-29 13:41:00 +0900

    refactor(glx): rename fileinfo to bufinfo.lua

--- .config/nvim/dein/rc/galaxyline/pos.lua
+++ .config/nvim/dein/rc/galaxyline/pos.lua
@@ -20,7 +20,7 @@ local colors = require('rc.galaxyline.colors')
 
 local Mode = require('rc.galaxyline.mode')
 local VCS = require('rc.galaxyline.vcs')
-local FileInfo = require('rc.galaxyline.fileinfo')
+local BufInfo = require('rc.galaxyline.bufinfo')
 
 -- To keep the order, set items line by line.
 gls.left[0] = Mode.ViMode
@@ -32,15 +32,14 @@ gls.left[13] = VCS.DiffAdd
 gls.left[14] = VCS.DiffModified
 gls.left[15] = VCS.DiffRemove
 
-gls.left[20] = FileInfo.FileIcon
-gls.left[21] = FileInfo.FileName
+gls.left[20] = BufInfo.FileIcon
+gls.left[21] = BufInfo.FileName
 
-gls.right[2] = FileInfo.LineInfo
+gls.right[2] = BufInfo.LineInfo
 
 -- Note: Apply the same name both to the key and to hl_group: FooBar to GalaxyFooBar.
-gls.short_line_left[0] = FileInfo.FileType
-
-gls.short_line_left[1] = FileInfo.FileIcon
-gls.short_line_left[2] = FileInfo.SFileName
+gls.short_line_left[0] = BufInfo.FileType
+gls.short_line_left[3] = BufInfo.FileIcon
+gls.short_line_left[4] = BufInfo.SFileName
 
 -- vim:path^=./module
