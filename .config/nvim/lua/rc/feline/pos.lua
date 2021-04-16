50dbdb6da1b1a4e2c610d94c3332cb39d2172767
kaile256 <46470475+kaile256@users.noreply.github.com>
2021-04-16 11:16:12 +0900

    feat(dein): use feline.nvim for statusline

---
+++ .config/nvim/lua/rc/feline/pos.lua
@@ -0,0 +1,27 @@
+-- TOML: appearance.toml
+-- Repo: famiu/feline.nvim
+
+local vi_mode_colors = {
+  NORMAL = 'green',
+  OP = 'green',
+  INSERT = 'red',
+  VISUAL = 'skyblue',
+  BLOCK = 'skyblue',
+  REPLACE = 'violet',
+  ['V-REPLACE'] = 'violet',
+  ENTER = 'cyan',
+  MORE = 'cyan',
+  SELECT = 'orange',
+  COMMAND = 'green',
+  SHELL = 'green',
+  TERM = 'green',
+  NONE = 'yellow'
+}
+
+require('feline').setup({
+  default_bg = '#1F1F23',
+  default_fg = '#D0D0D0',
+  components = require('rc.feline.components'),
+  properties = require('rc.feline.properties'),
+  vi_mode_colors = vi_mode_colors,
+})
---
+++ .config/nvim/lua/rc/feline/properties.lua
@@ -0,0 +1,25 @@
+-- TOML: appearance.toml
+-- Repo: famiu/feline.nvim
+
+local properties = {
+  force_inactive = {
+    filetypes = {},
+    buftypes = {},
+    bufnames = {},
+  }
+}
+
+properties.force_inactive.filetypes = {
+  'NvimTree',
+  'dbui',
+  'packer',
+  'startify',
+  'fugitive',
+  'fugitiveblame'
+}
+
+properties.force_inactive.buftypes = {
+  'terminal'
+}
+
+return properties
