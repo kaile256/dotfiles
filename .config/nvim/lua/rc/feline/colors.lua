4d1464310e192aea7152d35c5fb670ed0d37bb70
kaile256 <46470475+kaile256@users.noreply.github.com>
2021-04-16 13:59:31 +0900

    feat(feline): copy default values

---
+++ .config/nvim/lua/rc/feline/colors.lua
@@ -0,0 +1,23 @@
+-- TOML: appearance.toml
+-- Repo: famiu/feline.nvim
+
+local colors = {
+  fg = '#D0D0D0',
+  bg = '#1F1F23',
+
+  black = '#1B1B1B',
+  white = '#FFFFFF',
+
+  skyblue   = '#50B0F0',
+  cyan      = '#009090',
+  oceanblue = '#0066cc',
+  green     = '#60A040',
+
+  red     = '#D10000',
+  magenta = '#C26BDB',
+  violet  = '#9E93E8',
+  orange  = '#FF9000',
+  yellow  = '#E1E120',
+}
+
+return colors
---
+++ .config/nvim/lua/rc/feline/signs.lua
@@ -0,0 +1,38 @@
+-- TOML: appearance.toml
+-- Repo: famiu/feline.nvim
+
+local signs = {
+  separator = {},
+}
+
+signs.separator = {
+  vertical_bar = '┃',
+  vertical_bar_thin = '│',
+  block = '█',
+  circle = '●',
+
+  left = '',
+  right = '',
+
+  left_filled = '',
+  right_filled = '',
+
+  slant_left = '',
+  slant_left_thin = '',
+  slant_right = '',
+  slant_right_thin = '',
+
+  slant_left_2 = '',
+  slant_left_2_thin = '',
+
+  slant_right_2 = '',
+  slant_right_2_thin = '',
+
+  left_rounded = '',
+  left_rounded_thin = '',
+
+  right_rounded = '',
+  right_rounded_thin = '',
+}
+
+return signs
