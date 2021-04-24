93eadba4974256385509983af92b023f7233f3b3
kaile256 <46470475+kaile256@users.noreply.github.com>
2021-04-24 17:46:32 +0900

    refactor(feline): put related config together

--- .config/nvim/lua/rc/feline/pos.lua
+++ .config/nvim/lua/rc/feline/pos.lua
@@ -25,9 +25,10 @@ local vi_mode_colors = {
 }
 
 require('feline').setup({
-  default_bg = '#1F1F23',
-  default_fg = '#D0D0D0',
   components = require('rc.feline.components'),
   properties = require('rc.feline.properties'),
+
+  default_bg = '#1F1F23',
+  default_fg = '#D0D0D0',
   vi_mode_colors = vi_mode_colors,
 })
