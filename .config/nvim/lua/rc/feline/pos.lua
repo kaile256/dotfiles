5cfb8353e902964435a119c35ee051c9cdf656e6
kaile256 <46470475+kaile256@users.noreply.github.com>
2021-04-25 16:42:05 +0900

    docs(feline): add note at `colors`

--- .config/nvim/lua/rc/feline/pos.lua
+++ .config/nvim/lua/rc/feline/pos.lua
@@ -16,6 +16,9 @@ require('feline').setup({
   default_fg = colors.fg,
   default_bg = colors.bg,
 
+  -- Note: In writing my components, set colors via table, not in string whereby feline sets colors internally, because
+  -- lsp supports me in completion. Still, this ovewriting config at `colors` is required for rest components provided
+  -- by feline.
   colors = colors.vivid,
   -- vi_mode_colors = require('rc.feline.utils.vi_mode').colors_for_builtin,
 })
