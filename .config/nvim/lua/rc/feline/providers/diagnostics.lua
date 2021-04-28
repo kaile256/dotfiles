d50d6b117be0d8dfd8c1acf46ff3a6fe28fbf500
kaile256 <46470475+kaile256@users.noreply.github.com>
2021-04-28 13:41:07 +0900

    fix(feline): set default values for diagnostics counts

--- .config/nvim/lua/rc/feline/providers/diagnostics.lua
+++ .config/nvim/lua/rc/feline/providers/diagnostics.lua
@@ -16,7 +16,16 @@ local diagnostics = {
 
 local diagnostics_counts = function (bufnr)
   bufnr = bufnr or vim.fn.bufnr()
-  return vim.fn['ale#statusline#Count'](bufnr) or {}
+  local default = {
+    error = 0,
+    hint = 0,
+    warning = 0,
+    style_error = 0,
+    style_warning = 0,
+  }
+  -- Note: Without merging with default, ale#statusline#Count() possibly returns empty dictionary.
+  local counts = vim.tbl_extend('keep', vim.fn['ale#statusline#Count'](bufnr), default)
+  return counts
 end
 
 diagnostics.error = function (bufnr)
