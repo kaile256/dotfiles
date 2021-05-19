59bcb1e1372032d61eae663c861035e9eef82fa7
kaile256 <46470475+kaile256@users.noreply.github.com>
2021-05-19 14:34:49 +0900

    feat(session-tree): add config

---
+++ .config/nvim/dein/rc/session-tree/sou.lua
@@ -0,0 +1,13 @@
+-- TOML: default.toml
+-- Repo: chinnkarahoi/vim-session-tree
+
+vim.g.session_tree_count_limit = 15
+
+vim.g.close_unlisted_ignored_filetype = {
+  'agit',
+  'defx',
+  'gitcommit',
+  'pullrequest',
+  'tagbar',
+}
+
