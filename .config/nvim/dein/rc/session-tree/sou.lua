4be92922fc2b44f31d3047be7ceefca857ca68c4
kaile256 <46470475+kaile256@users.noreply.github.com>
2021-05-20 16:36:28 +0900

    refactor(session-tree): add ignored filetypes

--- .config/nvim/dein/rc/session-tree/sou.lua
+++ .config/nvim/dein/rc/session-tree/sou.lua
@@ -5,9 +5,11 @@ vim.g.session_tree_count_limit = 15
 
 vim.g.close_unlisted_ignored_filetype = {
   'agit',
+  'agit_diff',
+  'agit_stat',
   'defx',
   'gitcommit',
   'pullrequest',
   'tagbar',
+  'vista',
 }
-
