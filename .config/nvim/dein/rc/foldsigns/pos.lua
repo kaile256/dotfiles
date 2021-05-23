d40aafce50aa40a095d32b5094d2ffda9a0cf1b7
kaile256 <46470475+kaile256@users.noreply.github.com>
2021-05-23 13:45:43 +0900

    feat(dein): activate foldsigns.nvim

---
+++ .config/nvim/dein/rc/foldsigns/pos.lua
@@ -0,0 +1,8 @@
+-- TOML: fold.toml
+-- Repo: lewis6991/foldsigns.nvim
+
+-- Note: You can get sign names by `sign_getplaced(bufnr(), {'group': '*'})`
+require('foldsigns').setup {
+  -- include = nil, -- List of Lua patterns, nil to any signs.
+  -- exclude = nil, -- List of Lua patterns.
+}
