9eab83274e9e0e78607757a3f37f1c6b037bc05c
kaile256 <46470475+kaile256@users.noreply.github.com>
2021-05-24 18:40:22 +0900

    refactor(tele): modify mappings

--- .config/nvim/dein/rc/telescope/pos.lua
+++ .config/nvim/dein/rc/telescope/pos.lua
@@ -109,16 +109,16 @@ builtin.git_branches {
     actions.select_default:replace(actions.git_checkout)
 
     map('i', action_prefix .. 'd', actions.git_delete_branch)
-    map('n', action_prefix .. 'd', actions.git_delete_branch)
+    map('n', 'dd', actions.git_delete_branch)
 
     map('i', action_prefix .. 't', actions.git_track_branch)
     map('n', action_prefix .. 't', actions.git_track_branch)
 
     map('i', action_prefix .. 'r', actions.git_rebase_branch)
-    map('n', action_prefix .. 'r', actions.git_rebase_branch)
+    map('n', 'ri', actions.git_rebase_branch)
 
     map('i', action_prefix .. 'a', actions.git_create_branch)
-    map('n', action_prefix .. 'a', actions.git_create_branch)
+    map('n', 'gc', actions.git_create_branch)
 
     return true
   end
