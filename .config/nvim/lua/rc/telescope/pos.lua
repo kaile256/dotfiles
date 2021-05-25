3932f5e79a7f1a306796f70549355dbddb9317a2
kaile256 <46470475+kaile256@users.noreply.github.com>
2021-05-25 09:20:15 +0900

    refactor(tele): comment out deprecated previewers config

--- .config/nvim/lua/rc/telescope/pos.lua
+++ .config/nvim/lua/rc/telescope/pos.lua
@@ -44,13 +44,11 @@ require('telescope').setup{
     color_devicons = true,
     use_less = true,
     set_env = { ['COLORTERM'] = 'truecolor' }, -- default { }, currently unsupported for shells like cmd.exe / powershell.exe
-    file_previewer = require'telescope.previewers'.cat.new, -- For buffer previewer use `require'telescope.previewers'.vim_buffer_cat.new`
-    grep_previewer = require'telescope.previewers'.vimgrep.new, -- For buffer previewer use `require'telescope.previewers'.vim_buffer_vimgrep.new`
-    qflist_previewer = require'telescope.previewers'.qflist.new, -- For buffer previewer use `require'telescope.previewers'.vim_buffer_qflist.new`
 
-    -- Developer configurations: Not meant for general override
-    buffer_previewer_maker = require'telescope.previewers'.buffer_previewer_maker,
-
+    -- file_previewer = require'telescope.previewers'.cat.new,
+    -- grep_previewer = require'telescope.previewers'.vimgrep.new,
+    -- qflist_previewer = require'telescope.previewers'.qflist.new,
+    -- buffer_previewer_maker = require'telescope.previewers'.buffer_previewer_maker,
 
     mappings = {
       -- Ref: telescope/mappings.lua
