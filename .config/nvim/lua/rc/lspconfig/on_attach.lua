3870124a9c913c66601dfe9a2e87a02011f67b39
kaile256 <46470475+kaile256@users.noreply.github.com>
2021-05-30 19:06:35 +0900

    refactor(lsp): map keys via table

--- .config/nvim/lua/rc/lspconfig/on_attach.lua
+++ .config/nvim/lua/rc/lspconfig/on_attach.lua
@@ -9,36 +9,50 @@ if is_repmo_enabled then
 end
 
 local set_keymaps = function(client, bufnr)
+  local maps = {
+    n = {
+      gd = '<Cmd>lua vim.lsp.buf.definition()<CR>',
+      gD = '<Cmd>lua vim.lsp.buf.declaration()<CR>',
+      gr = '<Cmd>lua vim.lsp.buf.references()<CR>',
+      gR = '<Cmd>lua vim.lsp.buf.implementation()<CR>',
+      gy = '<Cmd>lua vim.lsp.buf.type_definition()<CR>',
+      cs = '<Cmd>lua vim.lsp.buf.rename()<CR>',
 
-  local buf_set_keymap = function(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
+      ['<C-]>'] = '<Cmd>lua vim.lsp.buf.hover()<CR>',
+      ['<C-k>'] = '<Cmd>lua vim.lsp.buf.signature_help()<CR>',
+
+      -- '<space>wa' = '<Cmd>lua vim.lsp.buf.add_workspace_folder()<CR>',
+      -- '<space>wr' = '<Cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>',
+      -- '<space>wl' = '<Cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>',
+
+      ['[x'] = { prev_diagnostic, { noremap=false, nowait=true, expr=true } },
+      [']x'] = { next_diagnostic, { noremap=false, nowait=true, expr=true } },
+      gx = '<Cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>',
+      gX = '<Cmd>lua vim.lsp.diagnostic.set_loclist()<CR>',
+
+      ['='] = client.resolved_capabilities.document_range_formatting and '<Cmd>lua vim.lsp.buf.range_formatting()<CR>',
+      ['=='] = client.resolved_capabilities.document_formatting and '<Cmd>lua vim.lsp.buf.formatting()<CR>',
+    },
+
+    x = {
+      ['='] = client.resolved_capabilities.document_range_formatting and '<Cmd>lua vim.lsp.buf.range_formatting()<CR>'
+    }
+  }
 
   local opts = { noremap=true, silent=true, nowait=true }
-  buf_set_keymap('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
-  buf_set_keymap('n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-  buf_set_keymap('n', 'gr', '<Cmd>lua vim.lsp.buf.references()<CR>', opts)
-  buf_set_keymap('n', 'gR', '<Cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-  buf_set_keymap('n', 'gy', '<Cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
-  buf_set_keymap('n', 'cs', '<Cmd>lua vim.lsp.buf.rename()<CR>', opts)
-
-  buf_set_keymap('n', '<C-]>', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
-  buf_set_keymap('n', '<C-k>', '<Cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
-
-  -- buf_set_keymap('n', '<space>wa', '<Cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
-  -- buf_set_keymap('n', '<space>wr', '<Cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
-  -- buf_set_keymap('n', '<space>wl', '<Cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
-
-  buf_set_keymap('n', '[x', prev_diagnostic, { noremap=false, nowait=true, expr=true })
-  buf_set_keymap('n', ']x', next_diagnostic, { noremap=false, nowait=true, expr=true })
-  buf_set_keymap('n', 'gx', '<Cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
-  buf_set_keymap('n', 'gX', '<Cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
-
-
-  if client.resolved_capabilities.document_formatting then
-    buf_set_keymap('n', '==', '<Cmd>lua vim.lsp.buf.formatting()<CR>', opts)
-  end
-  if client.resolved_capabilities.document_range_formatting then
-    buf_set_keymap('n', '=', '<Cmd>lua vim.lsp.buf.range_formatting()<CR>', opts)
-    buf_set_keymap('x', '=', '<Cmd>lua vim.lsp.buf.range_formatting()<CR>', opts)
+  for mode, key2command in pairs(maps) do
+    local buf_set_keymap = function(...) vim.api.nvim_buf_set_keymap(bufnr, mode, ...) end
+    for key, command in pairs(key2command) do
+      if command then
+        ---@diagnostic disable-next-line:redefined-local
+        local opts = opts
+        if type(command) == "table" then
+          opts = command[2]
+          command = command[1]
+        end
+        buf_set_keymap(key, command, opts)
+      end
+    end
   end
 end
 
