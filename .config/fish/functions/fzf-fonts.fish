5b926f9c823d6d26a96aa5f80285b4ae93f2d416
kaile256 <46470475+kaile256@users.noreply.github.com>
2021-06-01 07:44:15 +0900

    refactor(fish): simpler fzf-fonts

--- .config/fish/functions/fzf-fonts.fish
+++ .config/fish/functions/fzf-fonts.fish
@@ -1,4 +1,3 @@
 function fzf-fonts -d "echo font's path through fzf"
-    #fc-list | fzf | xargs | cut -d':' -f1 | sed -e 's/^\|$/"/g'
-    fc-list | fzf | xargs | cut -d':' -f1 | sed -e 's/ /\\\ /g'
+    fc-list -f "%{family}:%{style}\n" | fzf
 end
