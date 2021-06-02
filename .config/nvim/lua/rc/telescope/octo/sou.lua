12346b01e6a732293c267ae499299490119eb66b
kaile256 <46470475+kaile256@users.noreply.github.com>
2021-06-02 14:51:50 +0900

    refactor(octo): realign config tables

--- .config/nvim/lua/rc/telescope/octo/sou.lua
+++ .config/nvim/lua/rc/telescope/octo/sou.lua
@@ -4,19 +4,24 @@
 local config = {
   mappings = require"rc.telescope.octo.mappings",
 
-  date_format = "%Y %b %d %I:%M %p %Z";    -- date format
-  default_remote = {"upstream", "origin"}; -- order to try remotes
-  reaction_viewer_hint_icon = "";         -- marker for user reactions
-  user_icon = " ";                        -- user icon
-  timeline_marker = "";                   -- timeline marker
-  timeline_indent = "2";                   -- timeline indentation
-  right_bubble_delimiter = "";            -- Bubble delimiter
-  left_bubble_delimiter = "";             -- Bubble delimiter
-  github_hostname = "";                    -- GitHub Enterprise host
-  snippet_context_lines = 4;               -- number or lines around commented lines
+  date_format    = "%Y %b %d %I:%M %p %Z",
+  default_remote = {"upstream", "origin"}, -- order to try remotes
+
+  user_icon = " ",
+  reaction_viewer_hint_icon = "",
+
+  timeline_marker = "",
+  timeline_indent = "2",
+
+  right_bubble_delimiter = "",
+  left_bubble_delimiter  = "",
+
+  github_hostname       = "", -- GitHub Enterprise host
+  snippet_context_lines = 4,  -- number or lines around commented lines
+
   file_panel = {
-    size = 10,                             -- changed files panel rows
-    use_icons = true                       -- use web-devicons in file panel
+    size      = 10,   -- changed files panel rows
+    use_icons = true, -- use web-devicons in file panel
   },
 }
 
