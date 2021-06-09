805cb615d2401864a9b67fd13111aa12ac326664
kaile256 <46470475+kaile256@users.noreply.github.com>
2021-06-09 16:32:05 +0900

    feat(dein): install plugins for Zettelkasten

---
+++ .config/nvim/dein/rc/zk/sou.lua
@@ -0,0 +1,13 @@
+-- Repo: megalithic/zk.nvim
+-- Fork: kaile256/zk.nvim
+-- TOML: memo.toml
+
+require"zk".setup {
+  debug = false,
+  log = false,
+
+  default_keymaps = false,
+  default_notebook_path = "~/neuron",
+  fuzzy_finder = "telescope",
+  link_format = "markdown",
+}
