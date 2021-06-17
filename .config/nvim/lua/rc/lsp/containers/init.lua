69a0eefa504b6f27f7122723c093bd1a1198f13b
kaile256 <46470475+kaile256@users.noreply.github.com>
2021-06-17 18:13:54 +0900

    refactor(lsp): extract func to replace with latest container

--- .config/nvim/lua/rc/lsp/containers/init.lua
+++ .config/nvim/lua/rc/lsp/containers/init.lua
@@ -6,19 +6,24 @@ local Containers = {}
 ---@note Some languageserver might demand to sync volume mount on particular container. See lspcontainers.nvim/README.md.
 local is_lspcontainers_available, lspcontainers = pcall(require, 'lspcontainers')
 
+local replace_with_latest_container = function(cmd_sequence)
+  local pat_container_name = "^(lspcontainers/%S-):[0-9.]-$"
+  for idx, cmd in pairs(cmd_sequence) do
+    if cmd:match(pat_container_name) then
+      local container_name = cmd
+      local latest_container = container_name:gsub(pat_container_name, "%1:latest")
+      cmd_sequence[idx] = latest_container
+    end
+  end
+  return cmd_sequence
+end
+
 --- This function is supposed to be used in rc/lsp/config/pos.lua.
 Containers.lspcontainer_or_cmd = function(server, cmd)
   if not is_lspcontainers_available then return cmd end
   local enabled, container_cmd_sequence = pcall(lspcontainers.command, server)
   if not enabled then return cmd end
-  local pat_container_name = "^(lspcontainers/%S-):[0-9.]-$"
-  for idx, container_cmd in pairs(container_cmd_sequence) do
-    if container_cmd:match(pat_container_name) then
-      local container_name = container_cmd
-      local latest_container = container_name:gsub(pat_container_name, "%1:latest")
-      container_cmd_sequence[idx] = latest_container
-    end
-  end
+  container_cmd_sequence = replace_with_latest_container(container_cmd_sequence)
   return container_cmd_sequence
 end
 
