048e5e729157272dc7c906cd98af5ced3105027e
kaile256 <46470475+kaile256@users.noreply.github.com>
2021-06-17 16:42:35 +0900

    refactor(lsp): apply lspcontainer if available without further config

--- .config/nvim/lua/rc/lspconfig/pos.lua
+++ .config/nvim/lua/rc/lspconfig/pos.lua
@@ -44,10 +44,21 @@ local servers = {
   zk = require"rc.lspconfig.ls.zk",
 }
 
+--- TOML: lsp.toml
+--- Repo: lspcontainers/lspcontainers.nvim
+---@note Some languageserver might demand to sync volume mount on particular container. See lspcontainers.nvim/README.md.
+local is_lspcontainers_available, lspcontainers = pcall(require, 'lspcontainers')
+local lspcontainer_or_cmd = function(server, cmd)
+  if not is_lspcontainers_available then return cmd end
+  local enabled, container = pcall(lspcontainers.command, server)
+  return enabled and container or cmd
+end
+
 for server, config in pairs(servers) do
   local ls = lspconfig[server]
   if ls then
     config.on_attach = config.on_attach or on_attach
+    config.cmd = lspcontainer_or_cmd(server, config.cmd)
     ls.setup(config)
   else
     configs[server] = config -- Required to apply default_config.
