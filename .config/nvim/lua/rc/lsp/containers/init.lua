b539fe0719008450abde63bb0af102334180008d
kaile256 <46470475+kaile256@users.noreply.github.com>
2021-06-17 18:03:06 +0900

    refactor(lsp): prefer latest tagged lspcontainer

--- .config/nvim/lua/rc/lsp/containers/init.lua
+++ .config/nvim/lua/rc/lsp/containers/init.lua
@@ -9,8 +9,17 @@ local is_lspcontainers_available, lspcontainers = pcall(require, 'lspcontainers'
 --- This function is supposed to be used in rc/lsp/config/pos.lua.
 Containers.lspcontainer_or_cmd = function(server, cmd)
   if not is_lspcontainers_available then return cmd end
-  local enabled, container = pcall(lspcontainers.command, server)
-  return enabled and container or cmd
+  local enabled, container_cmd_sequence = pcall(lspcontainers.command, server)
+  if not enabled then return cmd end
+  local pat_container_name = "^(lspcontainers/%S-):[0-9.]-$"
+  for idx, container_cmd in pairs(container_cmd_sequence) do
+    if container_cmd:match(pat_container_name) then
+      local container_name = container_cmd
+      local latest_container = container_name:gsub(pat_container_name, "%1:latest")
+      container_cmd_sequence[idx] = latest_container
+    end
+  end
+  return container_cmd_sequence
 end
 
 return Containers
