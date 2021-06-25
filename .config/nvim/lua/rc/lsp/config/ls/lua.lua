15435d7ba19cbcfe455a3598fa2846c0121956d9
kaile256 <46470475+kaile256@users.noreply.github.com>
2021-06-25 14:40:44 +0900

    refactor(lsp/lua): remove dependency on lspcontainers

--- .config/nvim/lua/rc/lsp/config/ls/lua.lua
+++ .config/nvim/lua/rc/lsp/config/ls/lua.lua
@@ -2,15 +2,8 @@
 -- Repo: neovim/nvim-lspconfig
 -- Ref: lspconfig/sumneko_lua.lua
 
-local set_cmd = function()
-  local enabled, lspcontainers = pcall(require, 'lspcontainers')
-  return enabled
-    and lspcontainers.command('sumneko_lua')
-    or { '/usr/bin/lua-language-server' }
-end
-
 local config = {
-  cmd = set_cmd(),
+  cmd = { "/usr/bin/lua-language-server" },
 
   settings = {
     Lua = {
