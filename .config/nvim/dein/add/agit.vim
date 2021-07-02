75f4d14da4fba696a2e0b29f9a0afeb70a47d510
kaile256 <46470475+kaile256@users.noreply.github.com>
2021-07-02 13:13:55 +0900

    refactor(agit): define mappings in lua

--- .config/nvim/dein/add/agit.vim
+++
@@ -1,6 +0,0 @@
-" From: git.toml
-" Repo: cohama/agit.vim
-" Another: source/agit.vim
-
-nnoremap <silent> <space>gl :<c-u>AgitFile<cr>
-nnoremap <silent> <space>gL :<c-u>Agit<cr>
---
+++ .config/nvim/dein/rc/agit/add.lua
@@ -0,0 +1,6 @@
+-- From: git.toml
+-- Repo: cohama/agit.vim
+-- Another: source/agit.vim
+
+U.nnoremap({ "silent" }, "<space>gl", "<Cmd>AgitFile<CR>")
+U.nnoremap({ "silent" }, "<space>gL", "<Cmd>Agit<CR>")
