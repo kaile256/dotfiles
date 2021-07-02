b4c8f4d0cff4378bd16d25670b1ef4a82d682715
kaile256 <46470475+kaile256@users.noreply.github.com>
2021-07-02 21:50:23 +0900

    refactor(ls/zk): rewrite mappings using `U`

--- .config/nvim/lua/rc/lsp/config/ls/zk.lua
+++ .config/nvim/lua/rc/lsp/config/ls/zk.lua
@@ -3,11 +3,9 @@ local common_on_attach = require"rc.lsp.config.on_attach"
 
 local zk_mappings = function()
   if vim.fn['dein#tap']('zk.nvim') then
-    local opts = {noremap = true, silent = false, nowait = true}
-    vim.api.nvim_set_keymap("x", "<CR>", "<cmd>lua require('zk.command').create_note_link({})<cr>", opts)
-    vim.api.nvim_set_keymap("n", "<CR>",
-      "<cmd>lua require('zk.command').create_note_link({title = vim.fn.expand('<cword>')})<cr>",
-      opts)
+    U.xnoremap({ "buffer", "nowait" }, "<CR>", "<Cmd>lua require('zk.command').create_note_link({})<cr>")
+    U.nnoremap({ "buffer", "nowait" }, "<CR>",
+      "<Cmd>lua require('zk.command').create_note_link({title = vim.fn.expand('<cword>')})<cr>")
   end
 end
 
