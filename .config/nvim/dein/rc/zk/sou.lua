c48218838a93ec5fe7114c5752a4e7c7788e083a
kaile256 <46470475+kaile256@users.noreply.github.com>
2021-07-04 15:25:13 +0900

    docs(zk): add note

--- .config/nvim/dein/rc/zk/sou.lua
+++ .config/nvim/dein/rc/zk/sou.lua
@@ -6,7 +6,7 @@ require"zk".setup {
   debug = false,
   log = false,
 
-  default_keymaps = false,
+  default_keymaps = false, --- Map <CR> to `create_note_link()` in Normal/Visual mode with no check of duplication.
   default_notebook_path = vim.env.MY_MEMO .. "/neuron",
   fuzzy_finder = "telescope",
   link_format = "markdown",
