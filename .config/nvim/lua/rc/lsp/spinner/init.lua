559d0decc2e412adea67b0ca31eddad39b4bbaf5
kaile256 <46470475+kaile256@users.noreply.github.com>
2021-07-07 17:09:06 +0900

    refactor(lsp/spinner): show square when LS runs stably

--- .config/nvim/lua/rc/lsp/spinner/init.lua
+++ .config/nvim/lua/rc/lsp/spinner/init.lua
@@ -9,7 +9,7 @@ lsp_spinner.setup {
   spinner = {'⠋', '⠙', '⠹', '⠸', '⠼', '⠴', '⠦', '⠧', '⠇', '⠏'},
   interval = 80, -- spinner frame rate in ms
   redraw_rate = 100, -- max refresh rate of statusline in ms
-  placeholder = '  ', -- it will be displayed when there is no activity
+  placeholder = ' ',
 }
 
 local enable_work_done_progress = function()
