a295e2d7626e9a714d4bd8d8045d2219c20d1c51
kaile256 <46470475+kaile256@users.noreply.github.com>
2021-07-07 18:10:44 +0900

    feat(dap/go): add adapter config to directly use `delve`

--- .config/nvim/lua/rc/dap/ft/go.lua
+++ .config/nvim/lua/rc/dap/ft/go.lua
@@ -1,10 +1,35 @@
 local dap = require"dap"
 
-dap.adapters.go = {
+--- Ref: https://github.com/mfussenegger/nvim-dap/wiki/Debug-Adapter-installation#go-using-delve-directly
+---@diagnostic disable-next-line: unused-local
+local direct_delve_dap = function(callback, _)
+  local handle
+  local port = 38697
+  handle, _ = vim.loop.spawn(
+    "dlv",
+    {
+      args = { "dap", "-l", "127.0.0.1:" .. port },
+      detached = true
+    },
+    function(exit_code)
+      handle:close()
+      error("`delve` exited with exit code: " .. exit_code)
+    end)
+  vim.defer_fn(function()
+    callback({type = "server", host = "127.0.0.1", port = port})
+  end, 100)
+end
+
+-- https://github.com/go-delve/delve/blob/master/Documentation/usage/dlv_dap.md
+---@diagnostic disable-next-line: unused-local
+local adapter_on_vscode_go = {
   type = "executable";
   command = "node";
   args = { os.getenv("HOME") .. "/dev/golang/vscode-go/dist/debugAdapter.js" };
 }
+
+
+dap.adapters.go = direct_delve_dap
 
 dap.configurations.go = {
   {
