80ed4919b72009e37633b84fc095bcb4be75b179
kaile256 <46470475+kaile256@users.noreply.github.com>
2021-07-07 15:48:09 +0900

    refactor(wezterm): disable <C-z> on nvim

--- config/wezterm/my_keys.lua
+++ config/wezterm/my_keys.lua
@@ -6,6 +6,19 @@ local send_keys = function(key_sequence)
   return wezterm.action({ SendString = key_sequence })
 end
 
+--- CTRL-Z won't send `SIGTSTP` on nvim.
+--- FIXME: This script doens't work on the first nvim instance.
+local action_of_ctrl_z = function()
+  local this_pid = os.execute("xdotool getactivewindow getwindowpid | xargs pgrep -P")
+  local nvim_pid = os.execute("pgrep nvim")
+  if this_pid == nvim_pid then
+    return "Nop"
+  else
+    local sig_tstp = "\x1b[26;5u"
+    return send_keys(sig_tstp)
+  end
+end
+
 -- Get keycodes by `showkey -a`
 -- Modifiers: (http://www.leonerd.org.uk/hacks/fixterms/)
 -- S: 2u | M: 3u | M-S: 4u | C: 5u | C-S: 6u | C-M: 7u | C-M-S: 8u
@@ -28,6 +41,8 @@ local keys = {
 
   { key="Enter", mods="SHIFT", action=send_keys("\x1b[13;2u") }; -- <S-CR>
   { key="Enter", mods="CTRL",  action=send_keys("\x1b[13;5u") }; -- <C-CR>
+
+  { key="z", mods="CTRL", action=action_of_ctrl_z()};
 }
 
 local config = {
