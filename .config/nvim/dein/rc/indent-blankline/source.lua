c919d9f751a87085193b941956e287b048033287
kaile256 <46470475+kaile256@users.noreply.github.com>
2021-07-11 11:25:13 +0900

    refactor(indent): set colors from predefined palettes

--- .config/nvim/dein/rc/indent-blankline/source.lua
+++ .config/nvim/dein/rc/indent-blankline/source.lua
@@ -19,29 +19,57 @@ vim.g.indent_blankline_filetype_exclude = {
   "twiggy",
 }
 
-local color_table =
-  vim.g.colors_name == "spring-night"
-  and {
-    "#f2c6de",
-    "#f9c6c9",
-    "#faedcb",
-    "#c9e4de",
-    "#c6def1",
-    "#d1d6f1",
-    "#d8cdf0"
-  } or {
-    "#ff0000",
-    "#ffa500",
-    "#ffff00",
-    "#008000",
-    "#0000ff",
-    "#4b0082",
-    "#ee82ee"
-  }
+-- Note: The palettes derive primarily from https://coolors.co/334152.
+local color_palettes = {
+  square = {
+    "#334152",
+    "#523350",
+    "#524433",
+    "#335234",
+  },
+  monochromatic = {
+    -- "#334152",
+    "#4d637c",
+    "#6884a7",
+    "#82a6d1",
+    "#9dc8fc",
+    -- "#191f27",
+  },
+  tints = {
+    -- "#334152",
+    "#3d4e62",
+    "#485b73",
+    "#526883",
+    "#5d7594",
+    "#6a83a2",
+  },
+  analogus = {
+    "#334152",
+    "#334d52",
+    "#334752",
+    "#334152",
+    "#333b52",
+    "#333552",
+  },
+}
 
-local generate_highlight_list = function()
+-- Append all the items but the first and the last ones in reverse order, i.e., convert `{"foo", "bar", "baz", "qux"}`
+-- into `{"foo", "bar", "baz", "qux", "baz", "bar"}`.
+---@diagnostic disable-next-line: unused-function
+local tbl_make_back_and_forth = function(array)
+  local new_array = vim.deepcopy(array)
+  local i = #array - 1
+  while i > 1 do
+    table.insert(new_array, array[i])
+    i = i - 1
+  end
+  return new_array
+end
+
+-- Define vim's syntaxes, and return an array of highlights in the defined order.
+local generate_highlight_list = function(colors)
   local hl_list = {}
-  for idx, hex in pairs(color_table) do
+  for idx, hex in pairs(colors) do
     local prefix = "IndentBlankline"
     local syntax_name = prefix .. idx
     local options = "guibg=" .. hex .. " gui=nocombine"
@@ -50,7 +78,10 @@ local generate_highlight_list = function()
   end
   return hl_list
 end
-local highlight_list = generate_highlight_list()
+
+local color_table = tbl_make_back_and_forth(color_palettes.tints)
+
+local highlight_list = generate_highlight_list(color_table)
 vim.g.indent_blankline_char_highlight_list =  {}
 vim.g.indent_blankline_space_char_highlight_list = highlight_list
 
